47e1b0cdbdb2548a2cafaaf5f7f29a9c6a60d571
Max Hutchinson <maxhutch@gmail.com>
2014-08-21 20:24:16 -0500

    Cleaning drivers and added doxygen config file.
    drive and drive1 are fully implicit none'ed and use only'ed.  The
    first few routines in drive2 are as well, but it is a large file...

--- core/drive1.F90
+++ core/drive1.F90
@@ -1,124 +1,113 @@
+!> \file drive1.F90
+!! \brief top-level drivers for initialization, stepping, and cleanup
+!!
+!! Contains: nek_init, nek_solve, nek_advance, nek_end
 
-
-!-----------------------------------------------------------------------
-!> Initialize nek
+!> @brief initialize nek
 !!
 !! This includes reading the input files
-    subroutine nek_init(intracomm)
+subroutine nek_init(intracomm)
+  use kinds, only : DP
 
-    use ctimer
-    use size_m
-    use dealias, only : init_dealias
-    use domain, only : init_domain
-    use dxyz, only : init_dxyz
-    use esolv, only : init_esolv
-    use fdmh1, only : init_fdmh1
-    use geom, only : init_geom
-    use gmres, only : init_gmres
-    use hsmg, only : init_hsmg
-    use input, only : init_input
-    use ixyz, only : init_ixyz
-    use mass, only : init_mass
-    use mvgeom, only : init_mvgeom
-    use noncon, only : init_noncon
-    use parallel, only : init_parallel
-    use scratch, only : init_scratch
-    use semhat, only : init_semhat
-    use soln, only : init_soln
-    use topol, only : init_topol
-    use turbo, only : init_turbo
-    use zper, only : init_zper
+  use dealias, only : init_dealias
+  use domain, only : init_domain
+  use dxyz, only : init_dxyz
+  use esolv, only : init_esolv
+  use fdmh1, only : init_fdmh1
+  use geom, only : init_geom
+  use gmres, only : init_gmres
+  use hsmg, only : init_hsmg
+  use input, only : init_input
+  use ixyz, only : init_ixyz
+  use mass, only : init_mass
+  use mvgeom, only : init_mvgeom
+  use noncon, only : init_noncon
+  use parallel, only : init_parallel
+  use scratch, only : init_scratch
+  use semhat, only : init_semhat
+  use soln, only : init_soln
+  use topol, only : init_topol
+  use turbo, only : init_turbo
+  use zper, only : init_zper
 
-    use domain
-    use opctr
-    use dealias
-  use dxyz
-  use eigen
-  use esolv
-  use geom
-  use input
-  use ixyz
-  use mass
-  use mvgeom
-  use parallel
-  use soln
-  use steady
-  use topol
-  use tstep
-  use turbo
-  use wz_m
-  use wzf
-    use zper
+  use ctimer, only : etimes, dnekclock, etime1, ifsync, etims0, dnekclock_sync
+  use input, only : ifflow, iftran, solver_type
+  use soln, only : nid, jp
+  use tstep, only : istep, instep, nsteps, fintim, time
 
+  implicit none
 
-    real :: kwave2
-    real*8 :: t0, tpp
+  integer, intent(inout) :: intracomm
+  integer :: igeom
 
-    logical :: ifemati,ifsync_
+  real(DP) :: kwave2
+  real(DP) :: t0, tpp
 
-    call get_session_info(intracomm)
-          
-!     Initalize Nek (MPI stuff, word sizes, ...)
-!     call iniproc (initalized in get_session_info)
+  logical :: ifemati,ifsync_
 
-    etimes = dnekclock()
-    istep  = 0
-    tpp    = 0.0
+  call get_session_info(intracomm)
+        
+!   Initalize Nek (MPI stuff, word sizes, ...)
+!   call iniproc (initalized in get_session_info)
 
-    call opcount(1)
+  etimes = dnekclock()
+  istep  = 0
+  tpp    = 0.0
 
-!     Initialize and set default values.
-    call init_dealias()
-    call init_dxyz()
-    call init_domain()
-    call init_esolv()
-    call init_fdmh1()
-    call init_geom()
-    call init_gmres()
-    call init_hsmg()
-    call init_input()
-    call init_ixyz()
-    call init_mass()
-    call init_mvgeom()
-    call init_noncon()
-    call init_parallel()
-    call init_scratch()
-    call init_semhat()
-    call init_soln()
-    call init_topol()
-    call init_turbo()
-    call init_zper()
-    write(*,*) "Max inits"
+  call opcount(1)
 
-    call initdim
-    call initdat
-    call files
+!   Initialize and set default values.
+  call init_dealias()
+  call init_dxyz()
+  call init_domain()
+  call init_esolv()
+  call init_fdmh1()
+  call init_geom()
+  call init_gmres()
+  call init_hsmg()
+  call init_input()
+  call init_ixyz()
+  call init_mass()
+  call init_mvgeom()
+  call init_noncon()
+  call init_parallel()
+  call init_scratch()
+  call init_semhat()
+  call init_soln()
+  call init_topol()
+  call init_turbo()
+  call init_zper()
+  write(*,*) "Max inits"
 
-!     Read .rea +map file
-    etime1 = dnekclock()
-    call readat
+  call initdim
+  call initdat
+  call files
 
-    ifsync_ = ifsync
-    ifsync = .TRUE. 
+!   Read .rea +map file
+  etime1 = dnekclock()
+  call readat
 
-!     Initialize some variables
-    call setvar
+  ifsync_ = ifsync
+  ifsync = .TRUE. 
 
-!     Check for zero steps
-    instep=1
-    if (nsteps == 0 .AND. fintim == 0.) instep=0
+!   Initialize some variables
+  call setvar
 
-!     Setup domain topology
-    igeom = 2
-    call setup_topo
+!   Check for zero steps
+  instep=1
+  if (nsteps == 0 .AND. fintim == 0.) instep=0
 
-!     Compute GLL stuff (points, weights, derivate op, ...)
-    call genwz
+!   Setup domain topology
+  igeom = 2
+  call setup_topo
 
-!     Initalize io unit
-    call io_init
+!   Compute GLL stuff (points, weights, derivate op, ...)
+  call genwz
 
-!     Set size for CVODE solver
+!   Initalize io unit
+  call io_init
+
+!   Set size for CVODE solver
 #if 0
     if(ifcvode .AND. nsteps > 0) call cv_setsize(0,nfield)
 #endif
@@ -154,7 +143,7 @@
     call vrdsmsh
 
 !     Pressure solver initialization (uses "SOLN" space as scratch)
-    if (ifflow .AND. (fintime /= 0 .OR. nsteps /= 0)) then
+    if (ifflow .AND. (fintim /= 0 .OR. nsteps /= 0)) then
         call estrat
         if (iftran .AND. solver_type == 'itr') then
             call set_overlap
@@ -222,192 +211,169 @@
     return
     end subroutine nek_init
 !-----------------------------------------------------------------------
-    subroutine nek_solve
+!> \brief integrate the governing equations in time
+!!
+!! This routine includes the outer-most time loop
+subroutine nek_solve
+  use ctimer, only : ifsync
+  use tstep, only : instep, nid, istep, nsteps, lastep
+  implicit none
 
-    use ctimer
-    use size_m
-    use input
-    use tstep
+  real*4 :: papi_mflops
+  integer*8 :: papi_flops
 
-    real*4 :: papi_mflops
-    integer*8 :: papi_flops
+  integer :: isyc, itime, msteps, kstep
 
-    call nekgsync()
+  call nekgsync()
 
-    if (instep == 0) then
-        if(nid == 0) write(6,'(/,A,/,A,/)') &
-        ' nsteps=0 -> skip time loop', &
-        ' running solver in post processing mode'
-    else
-        if(nid == 0) write(6,'(/,A,/)') 'Starting time loop ...'
-    endif
+  if (instep == 0) then
+      if(nid == 0) write(6,'(/,A,/,A,/)') &
+      ' nsteps=0 -> skip time loop', &
+      ' running solver in post processing mode'
+  else
+      if(nid == 0) write(6,'(/,A,/)') 'Starting time loop ...'
+  endif
 
-    isyc  = 0
-    itime = 0
-    if(ifsync) isyc=1
-    itime = 1
-    call nek_comm_settings(isyc,itime)
+  isyc  = 0
+  itime = 0
+  if(ifsync) isyc=1
+  itime = 1
+  call nek_comm_settings(isyc,itime)
 
-    call nek_comm_startstat()
+  call nek_comm_startstat()
 
-    istep  = 0
-    msteps = 1
+  istep  = 0
+  msteps = 1
 
-    do kstep=1,nsteps,msteps
-        call nek__multi_advance(kstep,msteps)
-        call userchk
-        call prepost ( .FALSE. ,'his')
+  do kstep=1,nsteps,msteps
+      call nek__multi_advance(kstep,msteps)
+      call userchk
+      call prepost ( .FALSE. ,'his')
 !        call in_situ_check()
-        if (lastep == 1) goto 1001
-    enddo
-    1001 lastep=1
+      if (lastep == 1) goto 1001
+  enddo
+  1001 lastep=1
 
 
-    call nek_comm_settings(isyc,0)
+  call nek_comm_settings(isyc,0)
 
-    call comment
+  call comment
 
 !     check for post-processing mode
-    if (instep == 0) then
-        nsteps=0
-        istep=0
-        if(nid == 0) write(6,*) 'call userchk'
-        call userchk
-        if(nid == 0) write(6,*) 'done :: userchk'
-        call prepost ( .TRUE. ,'his')
-    else
-        if (nid == 0) write(6,'(/,A,/)') &
-        'end of time-step loop'
-    endif
+  if (instep == 0) then
+      nsteps=0
+      istep=0
+      if(nid == 0) write(6,*) 'call userchk'
+      call userchk
+      if(nid == 0) write(6,*) 'done :: userchk'
+      call prepost ( .TRUE. ,'his')
+  else
+      if (nid == 0) write(6,'(/,A,/)') &
+      'end of time-step loop'
+  endif
 
 
-    RETURN
-    end subroutine nek_solve
+  RETURN
+end subroutine nek_solve
 !-----------------------------------------------------------------------
-    subroutine nek_advance
+!> \brief take a single time-step
+!!
+!! Includes the primary Pn/Pn vs Pn/Pn-2 branch
+subroutine nek_advance
+  use input, only : iftran, ifmhd, ifsplit, ifheat, ifflow, param
+  implicit none
 
-    use ctimer
-    use size_m
-    use dealias
-  use dxyz
-  use eigen
-  use esolv
-  use geom
-  use input
-  use ixyz
-  use mass
-  use mvgeom
-  use parallel
-  use soln
-  use steady
-  use topol
-  use tstep
-  use turbo
-  use wz_m
-  use wzf
+  integer :: igeom
+  common /cgeom/ igeom
 
-    common /cgeom/ igeom
+  call nekgsync
+  IF (IFTRAN) CALL SETTIME
+  if (ifmhd ) call cfl_check
+  CALL SETSOLV
+  CALL COMMENT
 
-    call nekgsync
-    IF (IFTRAN) CALL SETTIME
-    if (ifmhd ) call cfl_check
-    CALL SETSOLV
-    CALL COMMENT
+  if (ifsplit) then   ! PN/PN formulation
 
-    if (ifsplit) then   ! PN/PN formulation
+      igeom = 1
+      if (ifheat)          call heat     (igeom)
+      call setprop
+      call qthermal
+      igeom = 1
+      if (ifflow)          call fluid    (igeom)
+      if (param(103) > 0) call q_filter(param(103))
+      call setup_convect (2) ! Save convective velocity _after_ filter
 
-        igeom = 1
-        if (ifheat)          call heat     (igeom)
-        call setprop
-        call qthermal
-        igeom = 1
-        if (ifflow)          call fluid    (igeom)
-        if (param(103) > 0) call q_filter(param(103))
-        call setup_convect (2) ! Save convective velocity _after_ filter
-
-    else                ! PN-2/PN-2 formulation
-      write(*,*) "Oops! Pn-2/Pn-2"
+  else                ! PN-2/PN-2 formulation
+    write(*,*) "Oops! Pn-2/Pn-2"
 #if 0
-        call setprop
-        do igeom=1,ngeom
+      call setprop
+      do igeom=1,ngeom
 
-            if (igeom > 2) call userchk_set_xfer
+          if (igeom > 2) call userchk_set_xfer
 
-            if (ifgeom) then
-                call gengeom (igeom)
-                call geneig  (igeom)
-            endif
+          if (ifgeom) then
+              call gengeom (igeom)
+              call geneig  (igeom)
+          endif
 
-            if (ifmhd) then
-                if (ifheat)      call heat     (igeom)
-                call induct   (igeom)
-            elseif (ifpert) then
-                write(*,*) "Oops! ifpert"
+          if (ifmhd) then
+              if (ifheat)      call heat     (igeom)
+              call induct   (igeom)
+          elseif (ifpert) then
+              write(*,*) "Oops! ifpert"
 #if 0
-                if (ifbase .AND. ifheat)  call heat          (igeom)
-                if (ifbase .AND. ifflow)  call fluid         (igeom)
-                if (ifflow)             call fluidp        (igeom)
-                if (ifheat)             call heatp         (igeom)
+              if (ifbase .AND. ifheat)  call heat          (igeom)
+              if (ifbase .AND. ifflow)  call fluid         (igeom)
+              if (ifflow)             call fluidp        (igeom)
+              if (ifheat)             call heatp         (igeom)
 #endif
-            else  ! std. nek case
-                if (ifheat)             call heat          (igeom)
-                if (ifflow)             call fluid         (igeom)
-                if (ifmvbd)             call meshv         (igeom)
-            endif
+          else  ! std. nek case
+              if (ifheat)             call heat          (igeom)
+              if (ifflow)             call fluid         (igeom)
+              if (ifmvbd)             call meshv         (igeom)
+          endif
 
-            if (igeom == ngeom .AND. param(103) > 0) &
-            call q_filter(param(103))
+          if (igeom == ngeom .AND. param(103) > 0) &
+          call q_filter(param(103))
 
-            call setup_convect (igeom) ! Save convective velocity _after_ filter
+          call setup_convect (igeom) ! Save convective velocity _after_ filter
 
-        enddo
+      enddo
 #endif
-    endif
+  endif
 
-    return
-    end subroutine nek_advance
+  return
+end subroutine nek_advance
 !-----------------------------------------------------------------------
-    subroutine nek_end
+!> \brief perform any end-of-run reporting
+subroutine nek_end
+  use parallel, only : xxth
+  use tstep, only : instep
+  implicit none
 
-    use size_m
-    use parallel
-    use opctr
-    use tstep
-
-    if(instep /= 0)  call runstat
-    if(xxth(1) > 0) call crs_stats(xxth(1))
-
-       
-!    call in_situ_end()
-    return
-    end subroutine nek_end
+  if(instep /= 0) call runstat
+  if(xxth(1) > 0) call crs_stats(xxth(1))
+     
+!  call in_situ_end()
+  return
+end subroutine nek_end
 !-----------------------------------------------------------------------
-    subroutine nek__multi_advance(kstep,msteps)
+!> \brief take a chunk of time-steps
+!! 
+!! Calls nek_advance in a loop
+subroutine nek__multi_advance(kstep,msteps)
+  use tstep, only : istep
+  implicit none
 
-    use size_m
-    use dealias
-  use dxyz
-  use eigen
-  use esolv
-  use geom
-  use input
-  use ixyz
-  use mass
-  use mvgeom
-  use parallel
-  use soln
-  use steady
-  use topol
-  use tstep
-  use turbo
-  use wz_m
-  use wzf
+  integer, intent(in) :: kstep
+  integer, intent(in) :: msteps
+  integer i
 
-    do i=1,msteps
-        istep = istep+i
-        call nek_advance
-    enddo
+  do i=1,msteps
+      istep = istep+i
+      call nek_advance
+  enddo
 
-    return
-    end subroutine nek__multi_advance
+  return
+end subroutine nek__multi_advance
 !-----------------------------------------------------------------------

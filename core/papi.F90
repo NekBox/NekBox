37118967776a35772a070f0b04d46af6285788e8
Max Hutchinson <maxhutch@gmail.com>
2014-09-02 22:33:06 -0500

    More cleaning.  no implicit through papi.F90.  Validated.

--- core/papi.F90
+++ core/papi.F90
@@ -1,24 +1,26 @@
-    subroutine nek_flops(flops,mflops)
-    real*4 :: rtime,ptime,mflops
-    integer*8 :: flops
+subroutine nek_flops(flops,mflops)
+  implicit none
+  real*4 :: rtime,ptime,mflops
+  integer*8 :: flops
 
-    call getflops_papi(flops,mflops)
+  call getflops_papi(flops,mflops)
 
-    return
-    end subroutine nek_flops
+  return
+end subroutine nek_flops
 
-    subroutine getflops_papi(flops,mflops)
+subroutine getflops_papi(flops,mflops)
+  implicit none
+  real*4 :: rtime,ptime,mflops
+  integer*8 :: flops
 #ifdef PAPI
-    include 'f77papi.h'
-    real*4 :: rtime,ptime,mflops
-    integer*8 :: flops
+  include 'f77papi.h'
 
-    call papif_flops(rtime,ptime,flops,mflops,ierr)
-    if(ierr > 0) then
-        flops = -1
-        mflops = -1
-    endif
+  call papif_flops(rtime,ptime,flops,mflops,ierr)
+  if(ierr > 0) then
+      flops = -1
+      mflops = -1
+  endif
 #endif
      
-    return
-    end subroutine getflops_papi
+  return
+end subroutine getflops_papi

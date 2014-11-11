00887a18eeede34145f1006029b6e3c1237f7462
Max Hutchinson <maxhutch@gmail.com>
2014-11-11 13:56:33 -0600

    Added rea flag, auto-detect word size, and cleaning.
    p69 > 0 signals to use this restart mode.  The variable load_name should
    be set in usrdat().

--- core/io_mod.F90
+++ core/io_mod.F90
@@ -1,80 +1,181 @@
+!==============================================================================
+!> \file io_mod.F90
+!! \brief Input/Output module with simplified interface
+!! \date November 2014
+!! \author Max Hutchinson
+!!
+!! This module should hold input/output routines, giving them implicit
+!! interfaces.
 module io
   implicit none
+ 
+  character(132) :: load_name !>!< name of output to load
 
+  public load_ic, load_name
+  private
 
 contains
 
-subroutine load_ic(word_size_load)
-  use kinds, only : r4, DP
-  use input, only : session
+!--------------------------------------------------------------------
+!> \brief Load initial condition from a previous multi-file output
+subroutine load_ic()
+  use kinds, only : DP
   use parallel, only : nid
   use soln, only : vx, vy, vz, t
-  use restart, only : pid0, fid0, wdsizo
+  use restart, only : pid0, fid0
   use size_m, only : nx1, ny1, nz1
-  integer, intent(in) :: word_size_load
-  character(132) :: session_pop
-  character(132) :: hname
+
   integer :: nelo !>!< number of i/o elements per io-node
-  integer :: wdsiztmp, ierr
+  integer :: word_size_load !>!< number of bytes per word
+
+  integer :: ierr
   real(DP), allocatable :: padding(:,:,:,:)
 
-#if 0
-  session_pop = session
-  session = prefix
   if (nid == pid0) then
-    call mfo_open_files('   ', ierr)
-  endi
-  session = session_pop
-#else
-  if (nid == pid0) then
-    hname = "io_test?.f00001"
-    call mbyte_open(hname, fid0, ierr)
+    call mbyte_open(load_name, fid0, ierr)
   endif
-#endif
-
-  wdsiztmp = wdsizo
-  wdsizo = word_size_load
 
   ! read and seek past header 
-  call mfo_read_hdr(nelo)
+  call mfo_read_header(nelo, word_size_load)
+
   ! seek past positions
   if (nid == pid0) then
     allocate(padding(nx1, ny1, nz1, nelo))
-    call byte_read(padding, wdsizo * size(padding) / 4, ierr)
-    call byte_read(padding, wdsizo * size(padding) / 4, ierr)
-    call byte_read(padding, wdsizo * size(padding) / 4, ierr)
+    call byte_read(padding, word_size_load * size(padding) / 4, ierr)
+    call byte_read(padding, word_size_load * size(padding) / 4, ierr)
+    call byte_read(padding, word_size_load * size(padding) / 4, ierr)
   endif
+
   ! read velocities
-  call mfo_inv(vx, vy, vz, size(vx,4), size(vx,1), size(vx,2), size(vx,3))    
+  call mfo_read_vector(vx, vy, vz, size(vx,4), size(vx,1), size(vx,2), size(vx,3), word_size_load) 
+
   ! seek past pressure
   if (nid == pid0) then
-    call byte_read(padding, wdsizo * size(padding) / 4, ierr)
+    call byte_read(padding, word_size_load * size(padding) / 4, ierr)
   endif
-  call mfo_ins(t(:,:,:,:,1), size(t, 4), size(t,1), size(t,2), size(t,3))
+  call mfo_read_scalar(t(:,:,:,:,1), size(t, 4), size(t,1), size(t,2), size(t,3), word_size_load)
 
   if (nid == pid0) then
     call byte_close(ierr)
   endif
 
-  wdsizo = wdsiztmp
-
 end subroutine load_ic
 
 !-----------------------------------------------------------------------
-!> \brief read a scalar field
-subroutine mfo_ins(u,nel,mx,my,mz)
-  use kinds, only : DP, r4
-  use size_m, only : nid, lelt, lxo
-  use restart, only : wdsizo, pid0, pid1
+!> \brief Read header and return number of elements and word size
+subroutine mfo_read_header(nelo,  word_size_file)
+  use kinds, only : r4, DP
+  use size_m, only : nid, nelt, lelt
+  use parallel, only : lglel, isize
+  use restart, only : nfileo, pid0, pid1
+  use restart, only : iHeaderSize
   implicit none
 
-  integer, intent(in) :: nel, mx, my, mz
+  integer, intent(out) :: nelo
+  integer, intent(out) :: word_size_file ! (intent out)
+
+  integer :: nelo_file !>!< number of i/o elements per io-node (from file)
+  real(r4) :: test_pattern
+  real(r4), allocatable :: padding(:)
+  integer :: lglist(0:lelt)
+  character(1) :: rdcode1(10)
+  integer :: fid0, istep, nelgt
+  real(DP) :: time
+  character(132) :: hdr
+
+  integer :: idum, nfileoo, j, mtype, inelp, ierr, i
+  integer :: len
+  integer :: pad_size
+  integer :: nxo, nyo, nzo
+
+  call nekgsync()
+  idum = 1
+
+  nfileoo = nfileo
+  if(nid == pid0) then                ! how many elements to dump
+      nelo = nelt
+      do j = pid0+1,pid1
+          mtype = j
+          call csend(mtype,idum,4,j,0)   ! handshake
+          call crecv(mtype,inelp,4)
+          nelo = nelo + inelp
+      enddo
+  else
+      mtype = nid
+      call crecv(mtype,idum,4)          ! hand-shake
+      call csend(mtype,nelt,4,pid0,0)   ! u4 :=: u8
+  endif
+
+  ierr = 0
+  if(nid == pid0) then
+      pad_size = (8 * (2**20) - (iHeaderSize + 4) ) / 4
+      allocate(padding(pad_size)); padding = 0.
+      call byte_read(hdr,iHeaderSize/4,ierr)
+      read(hdr, 1) word_size_file,nxo,nyo,nzo,nelo_file,nelgt,time,istep,fid0,nfileoo &
+      ,   (rdcode1(i),i=1,10)
+      1 format(5x,i1,1x,i2,1x,i2,1x,i2,1x,i10,1x,i10,1x,e20.13, &
+      &        1x,i9,1x,i6,1x,i6,1x,10a)
+      call byte_read(test_pattern,1,ierr)
+      ! pad up to 8MB
+      call byte_read(padding, pad_size, ierr)
+      deallocate(padding)
+  endif
+  call bcast(word_size_file, isize)
+
+  call err_chk(ierr,'Error writing header in mfo_write_hdr. $')
+
+  if(nid == pid0) then
+      call byte_read(lglel,nelt,ierr)
+      pad_size = -nelt
+      do j = pid0+1,pid1
+          mtype = j
+          call csend(mtype,idum,4,j,0)   ! handshake
+          len = 4*(lelt+1)
+          call crecv(mtype,lglist,len)
+          if(ierr == 0) then
+              call byte_read(lglist(1),lglist(0),ierr)
+          pad_size = pad_size - lglist(0)
+          endif
+      enddo
+
+    ! pad up to 8MB
+    do while (pad_size < 0) 
+      pad_size = pad_size + (8 * (2**20)) / 4
+    enddo
+    allocate(padding(pad_size)); padding = 0.
+    call byte_read(padding, pad_size, ierr)
+    deallocate(padding)
+
+  else
+      mtype = nid
+      call crecv(mtype,idum,4)          ! hand-shake
+              
+      lglist(0) = nelt
+      lglist(1:nelt) = lglel(1:nelt)
+
+      len = 4*(nelt+1)
+      call csend(mtype,lglist,len,pid0,0)
+  endif
+
+  call err_chk(ierr,'Error reading global nums in mfo_write_hdr$')
+  return
+end subroutine mfo_read_header
+
+!-----------------------------------------------------------------------
+!> \brief Read a scalar field
+subroutine mfo_read_scalar(u,nel,mx,my,mz, wdsizo)
+  use kinds, only : DP, r4
+  use size_m, only : nid, lelt, lxo
+  use restart, only : pid0, pid1
+  implicit none
+
+  integer, intent(in) :: nel, mx, my, mz, wdsizo
   real(DP), intent(in) :: u(mx,my,mz,1)
 
   real(r4), allocatable :: u4(:)
   real(DP), allocatable :: u8(:)
 
-  integer :: nxyz, len, leo, ntot, idum, ierr, nout, k, mtype
+  integer :: nxyz, ntot, idum, ierr, nout, k, mtype
 
   call nekgsync() ! clear outstanding message queues.
   if(mx > lxo .OR. my > lxo .OR. mz > lxo) then
@@ -83,8 +184,6 @@ subroutine mfo_ins(u,nel,mx,my,mz)
   endif
 
   nxyz = mx*my*mz
-  len  = 8 + 8*(lelt*nxyz)  ! recv buffer size
-  leo  = 8 + wdsizo*(nel*nxyz)
   ntot = nxyz*nel
 
   idum = 1
@@ -97,19 +196,18 @@ subroutine mfo_ins(u,nel,mx,my,mz)
   endif
 
   if (nid == pid0) then
-
+      idum = nel
+      nout = wdsizo/4 * nxyz * idum
       if(wdsizo == 4 .and. ierr == 0) then
-        nout = wdsizo/4 * ntot
         call byte_read(u4,nout,ierr)          ! u4 :=: u8
       elseif(ierr == 0) then
-        nout = wdsizo/4 * ntot
         call byte_read(u8,nout,ierr)          ! u4 :=: u8
       endif
 
       if (wdsizo == 4) then             ! 32-bit output
-          call copy4r (u,u4,ntot)
+          call copy4r (u,u4,nxyz * idum)
       else
-          call copy   (u,u8,ntot)
+          call copy   (u,u8,nxyz * idum)
       endif
 
 
@@ -120,12 +218,11 @@ subroutine mfo_ins(u,nel,mx,my,mz)
           call csend(mtype,idum,4,k,0)       ! handshake
           call crecv(mtype,idum,4)       ! handshake
 
+          nout  = wdsizo/4 * nxyz * idum
           if (wdsizo == 4 .AND. ierr == 0) then
-            nout  = wdsizo/4 * nxyz * idum
             call byte_read(u4,nout,ierr)
             call csend(mtype, u4, nout*4, k, 0)
           elseif(ierr == 0) then
-            nout  = wdsizo/4 * nxyz * idum 
             call byte_read(u8,nout,ierr)
             call csend(mtype, u8, nout*4, k, 0)
           endif
@@ -137,11 +234,11 @@ subroutine mfo_ins(u,nel,mx,my,mz)
       call csend(mtype, nel, 4, pid0, 0)
 
       if (wdsizo == 4) then             ! 32-bit output
-        call crecv(mtype, u4, ntot*wdsizo)
-        call copy4r (u, u4,ntot)
+        call crecv(mtype, u4, nxyz * nel *wdsizo)
+        call copy4r (u, u4, nxyz * nel)
       else
-        call crecv(mtype, u8, ntot*wdsizo)
-        call copy   (u,u8,ntot)
+        call crecv(mtype, u8, nxyz * nel *wdsizo)
+        call copy   (u, u8, nxyz * nel)
       endif
 
   endif
@@ -149,24 +246,24 @@ subroutine mfo_ins(u,nel,mx,my,mz)
   call err_chk(ierr,'Error writing data to .f00 in mfo_outs. $')
 
   return
-end subroutine mfo_ins
+end subroutine mfo_read_scalar
 
 !-----------------------------------------------------------------------
-!> \brief output a vector field
-subroutine mfo_inv(u,v,w,nel,mx,my,mz) 
+!> \brief Read a vector field
+subroutine mfo_read_vector(u,v,w,nel,mx,my,mz, wdsizo) 
   use kinds, only : DP, r4
   use size_m, only : nid, ndim, lxo, lelt
   use input, only : if3d
-  use restart, only : wdsizo, pid0, pid1
+  use restart, only : pid0, pid1
   implicit none
  
-  integer, intent(in) :: mx, my, mz
+  integer, intent(in) :: mx, my, mz, wdsizo
   real(DP), intent(in) :: u(mx*my*mz,*),v(mx*my*mz,*),w(mx*my*mz,*)
 
   real(r4), allocatable :: u4(:)
   real(DP), allocatable :: u8(:)
 
-  integer :: nxyz, len, leo, nel, idum, ierr
+  integer :: nxyz, nel, idum, ierr
   integer :: j, iel, nout, k, mtype
 
   call nekgsync() ! clear outstanding message queues.
@@ -176,8 +273,6 @@ subroutine mfo_inv(u,v,w,nel,mx,my,mz)
   endif
 
   nxyz = mx*my*mz
-  len  = 8 + 8*(lelt*nxyz*ndim)   ! recv buffer size (u4)
-  leo  = 8 + wdsizo*(nel*nxyz*ndim)
   idum = 1
   ierr = 0
 
@@ -188,7 +283,7 @@ subroutine mfo_inv(u,v,w,nel,mx,my,mz)
   endif
   
   if (nid == pid0) then
-      nout = wdsizo/4 * ndim*nel * nxyz
+      nout = wdsizo/4 * ndim * nel * nxyz
       if (wdsizo == 4 .and. ierr == 0) then
         call byte_read(u4,nout,ierr)          ! u4 :=: u8
       elseif (ierr == 0) then
@@ -273,100 +368,7 @@ subroutine mfo_inv(u,v,w,nel,mx,my,mz)
 
   call err_chk(ierr,'Error writing data to .f00 in mfo_outv. $')
   return
-end subroutine mfo_inv
-
-!-----------------------------------------------------------------------
-!> \brief write hdr, byte key, els.
-subroutine mfo_read_hdr(nelo)
-  use kinds, only : r4
-  use size_m, only : nid, nelt, lelt, ldimt
-  use input, only : ifxyo, ifvo, ifpo, ifto, ifpsco
-  use parallel, only : nelgt, lglel
-  use restart, only : nfileo, pid0, pid1, rdcode1, wdsizo, nxo, nyo, nzo
-  use restart, only : fid0, iHeaderSize
-  use tstep, only : istep, time
-  implicit none
-
-  integer, intent(out) :: nelo
-
-  real(r4) :: test_pattern
-  real(r4), allocatable :: padding(:)
-  integer :: lglist(0:lelt)
-
-  character(132) :: hdr
-
-  integer :: idum, nfileoo, j, mtype, inelp, ierr, i, npscalo, k
-  integer :: ibsw_out, len
-  integer :: pad_size
-
-  call nekgsync()
-  idum = 1
-
-  nfileoo = nfileo
-  if(nid == pid0) then                ! how many elements to dump
-      nelo = nelt
-      do j = pid0+1,pid1
-          mtype = j
-          call csend(mtype,idum,4,j,0)   ! handshake
-          call crecv(mtype,inelp,4)
-          nelo = nelo + inelp
-      enddo
-  else
-      mtype = nid
-      call crecv(mtype,idum,4)          ! hand-shake
-      call csend(mtype,nelt,4,pid0,0)   ! u4 :=: u8
-  endif
-
-  ierr = 0
-  if(nid == pid0) then
-      pad_size = (8 * (2**20) - (iHeaderSize + 4) ) / 4
-      allocate(padding(pad_size)); padding = 0.
-      call byte_read(hdr,iHeaderSize/4,ierr)
-      call byte_read(test_pattern,1,ierr)
-      ! pad up to 8MB
-      call byte_read(padding, pad_size, ierr)
-      deallocate(padding)
-  endif
-
-  call err_chk(ierr,'Error writing header in mfo_write_hdr. $')
-
-  if(nid == pid0) then
-      call byte_read(lglel,nelt,ierr)
-      pad_size = -nelt
-      do j = pid0+1,pid1
-          mtype = j
-          call csend(mtype,idum,4,j,0)   ! handshake
-          len = 4*(lelt+1)
-          call crecv(mtype,lglist,len)
-          if(ierr == 0) then
-              call byte_read(lglist(1),lglist(0),ierr)
-          pad_size = pad_size - lglist(0)
-          endif
-      enddo
-
-    ! pad up to 8MB
-    do while (pad_size < 0) 
-      pad_size = pad_size + (8 * (2**20)) / 4
-    enddo
-    allocate(padding(pad_size)); padding = 0.
-    call byte_read(padding, pad_size, ierr)
-    deallocate(padding)
-
-  else
-      mtype = nid
-      call crecv(mtype,idum,4)          ! hand-shake
-              
-      lglist(0) = nelt
-      lglist(1:nelt) = lglel(1:nelt)
-
-      len = 4*(nelt+1)
-      call csend(mtype,lglist,len,pid0,0)
-  endif
-
-  call err_chk(ierr,'Error reading global nums in mfo_write_hdr$')
-  return
-end subroutine mfo_read_hdr
-
+end subroutine mfo_read_vector
 
 end module io
 

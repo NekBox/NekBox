5a9486eb661a05d78234571ad10589fbc5943a8f
Max Hutchinson <maxhutch@gmail.com>
2014-11-14 15:16:34 -0600

    Pulled tests into their own routine to remove #ifdef's and recompiles.

--- core/poisson_mod.F90
+++ core/poisson_mod.F90
@@ -22,9 +22,6 @@ module poisson
 
 contains
 
-!#define UNITARY_TEST
-!#define SHUFFLE_TEST
-!#define NO_FFT
 !> \brief 
 subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
   use kinds, only : DP
@@ -77,34 +74,15 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
   nelm = size(rhs, 4)
   allocate(rhs_coarse(nelm))
   forall(i = 1 : nelm) rhs_coarse(i) = sum(bm1(:,:,:,i) * rhs(:,:,:,i))
-
-#if defined(SHUFFLE_TEST) || defined(UNITARY_TEST) || defined(NO_FFT)
-  do i = 1, nelm
-    rhs_coarse(i) = lglel(i)
-  enddo
-#endif
  
   ! reorder onto sticks
   allocate(plane_xy(0:shape_x(1)-1, 0:nin_local_xy-1, 0:nin_local_yz-1) )
 
   call mesh_to_grid(rhs_coarse, plane_xy, shape_x)
 
-#if defined(UNITARY_TESTT) || defined(NO_FFTT)
-  do idx = 0, shape_c(1)-1
-    do idy = idx_in_local_xy, idx_in_local_xy + nin_local_xy - 1
-      do idz = idx_in_local_yz, idx_in_local_yz + nin_local_yz - 1
-        ieg = 1 + idx + idy * shape_c(1) + idz * shape_c(1) * shape_c(2)
-        plane_xy(idx, idy-idx_in_local_xy, idz-idx_in_local_yz) = ieg
-      enddo
-    enddo
-  enddo
-#endif
-
   ! forward FFT
   rescale = 1._dp
-#ifndef NO_FFT
   call fft_r2r(plane_xy, shape_x(1), int(nin_local_xy * nin_local_yz), FFTW_R2HC, rescale)
-#endif
   
   allocate(plane_yx(0:shape_c(2)-1, 0:nout_local_xy-1, 0:nin_local_yz-1) )
   do i = 0, nin_local_yz - 1
@@ -116,26 +94,7 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
   enddo
   deallocate(plane_xy)
 
-#ifdef NO_FFT
-  do idx = 0, nout_local_xy - 1
-    do idy = 0, shape_x(2) - 1
-      do idz = 0, nin_local_yz - 1
-        ieg = 1 + idx + idx_out_local_xy + shape_x(1)*idy &
-                + shape_x(1) * shape_x(2) * (idz + idx_in_local_yz)
-        err = abs(plane_yx(idy,idx,idz) - ieg)
-        if (err > 0.001) then
-          write(*,'(A,6(I6))') "WARNING: confused about k after xy", nid, idx, idy, idz, ieg, int(plane_yx(idy,idx,idz))
-          return
-        endif
-      enddo
-    enddo
-  enddo
-  if (nid == 0) write(*,*) "Passed xy transpose"
-#endif
-
-#ifndef NO_FFT
   call fft_r2r(plane_yx, shape_x(2), int(nout_local_xy * nin_local_yz), FFTW_R2HC, rescale)
-#endif
 
   allocate(plane_zy(0:shape_c(3)-1, 0:nout_local_xy-1, 0:nout_local_yz-1) )
   do i = 0, nout_local_xy-1
@@ -147,29 +106,9 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
   enddo
   deallocate(plane_yx)
 
-#ifdef NO_FFT
-  do idx = 0, nout_local_xy - 1
-    do idy = 0, nout_local_yz - 1
-      do idz = 0, shape_x(3) - 1
-        ieg = 1 + idx + idx_out_local_xy + shape_x(1)*(idy + idx_out_local_yz) &
-                + shape_x(1) * shape_x(2) * idz
-        err = abs(plane_zy(idz,idx,idy) - ieg)
-        if (err > 0.001) then
-          write(*,'(A,6(I6))') "WARNING: confused about k after yz", nid, idx, idy, idz, ieg, int(plane_zy(idz,idy,idx))
-          return
-        endif
-      enddo
-    enddo
-  enddo
-  if (nid == 0) write(*,*) "Passed yz transpose"
-#endif
-
-#ifndef NO_FFT
   call fft_r2r(plane_zy, shape_x(3), int(nout_local_xy * nout_local_yz), FFTW_REDFT00, rescale)
-#endif
 
   ! Poisson kernel
-#if !(defined(UNITARY_TEST) || defined(SHUFFLE_TEST) || defined(NO_FFT))
   do idz = 0, shape_c(3)/nxy - 1
     do idy = 0, nout_local_yz - 1
       do idx = 0, nout_local_xy - 1
@@ -198,13 +137,9 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
       enddo
     enddo
   enddo
-#endif
-
 
   ! reverse FFT
-#ifndef NO_FFT
   call fft_r2r(plane_zy, shape_x(3), int(nout_local_xy * nout_local_yz), FFTW_REDFT00, rescale)
-#endif
 
   allocate(plane_yx(0:shape_c(2)-1, 0:nout_local_xy-1, 0:nin_local_yz-1) )
   do i = 0, nout_local_xy - 1
@@ -216,9 +151,7 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
   enddo
   deallocate(plane_zy)
 
-#ifndef NO_FFT
   call fft_r2r(plane_yx, shape_x(2), int(nout_local_xy * nin_local_yz), FFTW_HC2R, rescale)
-#endif
 
   allocate(plane_xy(0:shape_c(1)-1, 0:nin_local_xy-1, 0:nin_local_yz-1))
   do i = 0, nin_local_yz - 1
@@ -229,48 +162,17 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
     call fftw_mpi_execute_r2r(transpose_plan, plane_yx(:,:,i), plane_xy(:,:,i))
   enddo
 
-#ifndef NO_FFT
   call fft_r2r(plane_xy, shape_x(1), int(nin_local_xy * nin_local_yz), FFTW_HC2R, rescale)
-#endif
-
 
   plane_xy = plane_xy * (1._dp/ rescale)
-#ifdef UNITARY_TEST
-  do idx = 0, shape_x(1)-1
-    do idy = idx_in_local_xy, idx_in_local_xy + nin_local_xy - 1
-      do idz = idx_in_local_yz, idx_in_local_yz + nin_local_yz - 1
-        ieg = 1 + idx + idy * shape_x(1) + idz * shape_x(1) * shape_x(2)
-        err = abs(plane_xy(idx, idy-idx_in_local_xy, idz - idx_in_local_yz) - ieg)
-        if (err > 0.001) then
-          write(*,*) "WARNING: spectral transform not unitary", nid, idx, idy, idz, ieg, err
-          exit
-        endif
-      enddo
-    enddo
-  enddo
-  if (nid == 0) write(*,*) "Passed unitary test"
-#endif
 
-  allocate(soln_coarse(nelm)); soln_coarse = 0._dp
   ! reorder to local elements
+  allocate(soln_coarse(nelm)); soln_coarse = 0._dp
   call grid_to_mesh(plane_xy, soln_coarse, shape_x)
-
-#ifdef SHUFFLE_TEST
-  do i = 1, nelm
-    err = abs(soln_coarse(i) - lglel(i))
-    if (err > 0.001) then
-      write(*,*) "WARNING: shuffle not working", nid, i, nid*nelm + i, err
-      exit
-    endif
-  enddo
-  if (nid == 0) write(*,*) "Passed shuffle test"
-#endif
 
   ! populate U
   forall(i = 1 : nelm) u(:,:,:,i) = binvm1(:,:,:,i) * soln_coarse(i)
-#if defined(UNITARY_TEST) || defined(SHUFFLE_TEST) || defined(NO_FFT)
-  u = 0._dp
-#endif
+  !forall(i = 1 : nelm) u(:,:,:,i) =  soln_coarse(i)
 
   ! update residual
   allocate(tmp_fine(size(u,1), size(u,2), size(u,3), size(u,4)))
@@ -282,7 +184,8 @@ subroutine spectral_solve(u,rhs,h1,mask,mult,imsh,isd)
   if (nid == 0) write(*,*) "RHS before: ", sqrt(sum(rhs * rhs))
   RHS = RHS - tmp_fine 
   if (nid == 0) write(*,*) "RHS after : ", sqrt(sum(rhs * rhs))
- 
+
+  return
  
 end subroutine spectral_solve
 
@@ -320,6 +223,9 @@ subroutine init_comm_infrastructure(comm_world, shape_x)
   write(*,'(A,6(I5))') "MAX:", nid, alloc_local_xy, nin_local_xy, idx_in_local_xy, nout_local_xy, idx_out_local_xy
   call nekgsync()
   write(*,'(A,6(I5))') "MAX:", nid, alloc_local_yz, nin_local_yz, idx_in_local_yz, nout_local_yz, idx_out_local_yz
+
+  call transpose_test()
+  call shuffle_test()
 
   interface_initialized = .true.
 
@@ -448,6 +354,254 @@ subroutine grid_to_mesh(grid, mesh, shape_x)
   enddo
 end subroutine grid_to_mesh
 
+subroutine shuffle_test()
+  use kinds, only : DP
+  use size_m, only : nelv
+  use geom, only : bm1, binvm1
+  use mesh, only : shape_x, start_x, end_x
+  use parallel, only : nekcomm, nid, nekreal
+  use parallel, only : lglel, gllel, gllnid
+  use tstep, only : PI
 
+  use fftw3, only : fftw_mpi_local_size_many_transposed
+  use fftw3, only : fftw_plan_many_r2r
+  use fftw3, only : fftw_mpi_plan_many_transpose
+  use fftw3, only : FFTW_MPI_DEFAULT_BLOCK
+  use fftw3, only : FFTW_ESTIMATE, FFTW_R2HC, FFTW_HC2R, FFTW_REDFT00
+  use fftw3, only : fftw_execute_r2r, fftw_mpi_execute_r2r
+  use fft, only : fft_r2r
+  use mpif, only : MPI_STATUS_IGNORE
+
+
+  real(DP), allocatable :: rhs_coarse(:), soln_coarse(:)
+  real(DP), allocatable :: tmp_fine(:,:,:,:)
+  integer :: nelm
+  integer :: i, ieg, ierr
+  type(C_PTR) :: transpose_plan
+  type(C_PTR) :: dft_plan
+  integer(C_INTPTR_T) :: shape_c(3), dest_pid, src_pid
+  integer(C_INTPTR_T), parameter :: one = 1
+  integer :: ix(3), idx, idy, idz
+  real(DP), allocatable :: plane_xy(:,:,:), plane_yx(:,:,:), plane_zy(:,:,:)
+  integer, allocatable :: mpi_reqs(:)
+  integer :: n_mpi_reqs
+  real(DP) :: err
+  real(DP) :: rescale
+  real(DP) :: h2(1,1,1,1)
+  real(DP) :: kx, ky, kz
+
+  shape_c = shape_x
+
+  ! convert RHS to coarse mesh
+  nelm = nelv
+  allocate(rhs_coarse(nelm))
+  do i = 1, nelm
+    rhs_coarse(i) = lglel(i)
+  enddo
+ 
+  ! reorder onto sticks
+  allocate(plane_xy(0:shape_x(1)-1, 0:nin_local_xy-1, 0:nin_local_yz-1) )
+  call mesh_to_grid(rhs_coarse, plane_xy, shape_x)
+
+#if defined(UNITARY_TESTT) || defined(NO_FFTT)
+  do idx = 0, shape_c(1)-1
+    do idy = idx_in_local_xy, idx_in_local_xy + nin_local_xy - 1
+      do idz = idx_in_local_yz, idx_in_local_yz + nin_local_yz - 1
+        ieg = 1 + idx + idy * shape_c(1) + idz * shape_c(1) * shape_c(2)
+        plane_xy(idx, idy-idx_in_local_xy, idz-idx_in_local_yz) = ieg
+      enddo
+    enddo
+  enddo
+#endif
+
+  ! forward FFT
+  rescale = 1._dp
+  call fft_r2r(plane_xy, shape_x(1), int(nin_local_xy * nin_local_yz), FFTW_R2HC, rescale)
+  
+  allocate(plane_yx(0:shape_c(2)-1, 0:nout_local_xy-1, 0:nin_local_yz-1) )
+  do i = 0, nin_local_yz - 1
+    transpose_plan = fftw_mpi_plan_many_transpose( &
+                      shape_c(2), shape_c(1), one, &
+                      nin_local_xy, nout_local_xy, &
+                      plane_xy(:,:,i), plane_yx(:,:,i), comm_xy, FFTW_ESTIMATE)
+    call fftw_mpi_execute_r2r(transpose_plan, plane_xy(:,:,i), plane_yx(:,:,i))
+  enddo
+  deallocate(plane_xy)
+
+  call fft_r2r(plane_yx, shape_x(2), int(nout_local_xy * nin_local_yz), FFTW_R2HC, rescale)
+
+  allocate(plane_zy(0:shape_c(3)-1, 0:nout_local_xy-1, 0:nout_local_yz-1) )
+  do i = 0, nout_local_xy-1
+    transpose_plan = fftw_mpi_plan_many_transpose( &
+                      shape_c(3), shape_c(2), one, &
+                      FFTW_MPI_DEFAULT_BLOCK, FFTW_MPI_DEFAULT_BLOCK, &
+                      plane_yx(:,i,:), plane_zy(:,i,:), comm_yz, FFTW_ESTIMATE)
+    call fftw_mpi_execute_r2r(transpose_plan, plane_yx(:,i,:), plane_zy(:,i,:))
+  enddo
+  deallocate(plane_yx)
+
+  call fft_r2r(plane_zy, shape_x(3), int(nout_local_xy * nout_local_yz), FFTW_REDFT00, rescale)
+
+  ! reverse FFT
+  call fft_r2r(plane_zy, shape_x(3), int(nout_local_xy * nout_local_yz), FFTW_REDFT00, rescale)
+
+  allocate(plane_yx(0:shape_c(2)-1, 0:nout_local_xy-1, 0:nin_local_yz-1) )
+  do i = 0, nout_local_xy - 1
+  transpose_plan = fftw_mpi_plan_many_transpose( &
+                    shape_c(2), shape_c(3), one, &
+                    FFTW_MPI_DEFAULT_BLOCK, FFTW_MPI_DEFAULT_BLOCK, &
+                    plane_zy(:,i,:), plane_yx(:,i,:), comm_yz, FFTW_ESTIMATE)
+  call fftw_mpi_execute_r2r(transpose_plan, plane_zy(:,i,:), plane_yx(:,i,:))
+  enddo
+  deallocate(plane_zy)
+
+  call fft_r2r(plane_yx, shape_x(2), int(nout_local_xy * nin_local_yz), FFTW_HC2R, rescale)
+
+  allocate(plane_xy(0:shape_c(1)-1, 0:nin_local_xy-1, 0:nin_local_yz-1))
+  do i = 0, nin_local_yz - 1
+    transpose_plan = fftw_mpi_plan_many_transpose( &
+                      shape_c(1), shape_c(2), one, &
+                      FFTW_MPI_DEFAULT_BLOCK, FFTW_MPI_DEFAULT_BLOCK, &
+                      plane_yx(:,:,i), plane_xy(:,:,i), comm_xy, FFTW_ESTIMATE)
+    call fftw_mpi_execute_r2r(transpose_plan, plane_yx(:,:,i), plane_xy(:,:,i))
+  enddo
+
+  call fft_r2r(plane_xy, shape_x(1), int(nin_local_xy * nin_local_yz), FFTW_HC2R, rescale)
+
+
+  plane_xy = plane_xy * (1._dp/ rescale)
+
+  allocate(soln_coarse(nelm)); soln_coarse = 0._dp
+  ! reorder to local elements
+  call grid_to_mesh(plane_xy, soln_coarse, shape_x)
+
+  do i = 1, nelm
+    err = abs(soln_coarse(i) - lglel(i))
+    if (err > 0.001) then
+      write(*,*) "WARNING: shuffle not working", nid, i, nid*nelm + i, err
+      exit
+    endif
+  enddo
+  if (nid == 0) write(*,*) "Passed shuffle test" 
+end subroutine shuffle_test
+
+subroutine transpose_test()
+  use kinds, only : DP
+  use size_m, only : nelv
+  use geom, only : bm1, binvm1
+  use mesh, only : shape_x, start_x, end_x
+  use parallel, only : nekcomm, nid, nekreal
+  use parallel, only : lglel, gllel, gllnid
+  use tstep, only : PI
+
+  use fftw3, only : fftw_mpi_local_size_many_transposed
+  use fftw3, only : fftw_plan_many_r2r
+  use fftw3, only : fftw_mpi_plan_many_transpose
+  use fftw3, only : FFTW_MPI_DEFAULT_BLOCK
+  use fftw3, only : FFTW_ESTIMATE, FFTW_R2HC, FFTW_HC2R, FFTW_REDFT00
+  use fftw3, only : fftw_execute_r2r, fftw_mpi_execute_r2r
+  use fft, only : fft_r2r
+  use mpif, only : MPI_STATUS_IGNORE
+
+  real(DP), allocatable :: rhs_coarse(:), soln_coarse(:)
+  real(DP), allocatable :: tmp_fine(:,:,:,:)
+  integer :: nelm
+  integer :: i, ieg, ierr
+  type(C_PTR) :: transpose_plan
+  type(C_PTR) :: dft_plan
+  integer(C_INTPTR_T) :: shape_c(3), dest_pid, src_pid
+  integer(C_INTPTR_T), parameter :: one = 1
+  integer :: ix(3), idx, idy, idz
+  real(DP), allocatable :: plane_xy(:,:,:), plane_yx(:,:,:), plane_zy(:,:,:)
+  integer, allocatable :: mpi_reqs(:)
+  integer :: n_mpi_reqs
+  real(DP) :: err
+  real(DP) :: rescale
+  real(DP) :: h2(1,1,1,1)
+  real(DP) :: kx, ky, kz
+
+  shape_c = shape_x
+
+  ! convert RHS to coarse mesh
+  nelm = nelv
+  allocate(rhs_coarse(nelm))
+
+  do i = 1, nelm
+    rhs_coarse(i) = lglel(i)
+  enddo
+ 
+  ! reorder onto sticks
+  allocate(plane_xy(0:shape_x(1)-1, 0:nin_local_xy-1, 0:nin_local_yz-1) )
+
+  call mesh_to_grid(rhs_coarse, plane_xy, shape_x)
+
+  do idx = 0, shape_c(1)-1
+    do idy = idx_in_local_xy, idx_in_local_xy + nin_local_xy - 1
+      do idz = idx_in_local_yz, idx_in_local_yz + nin_local_yz - 1
+        ieg = 1 + idx + idy * shape_c(1) + idz * shape_c(1) * shape_c(2)
+        err = abs(plane_xy(idx, idy-idx_in_local_xy, idz-idx_in_local_yz) - ieg)
+        if (err > 0.001) then
+          write(*,'(A,6(I6))') "WARNING: confused about k after init", nid, idx, idy, idz, ieg, int(plane_yx(idy,idx,idz))
+          return
+        endif
+      enddo
+    enddo
+  enddo
+  if (nid == 0) write(*,*) "Passed init"
+
+  ! forward FFT
+  rescale = 1._dp
+  
+  allocate(plane_yx(0:shape_c(2)-1, 0:nout_local_xy-1, 0:nin_local_yz-1) )
+  do i = 0, nin_local_yz - 1
+    transpose_plan = fftw_mpi_plan_many_transpose( &
+                      shape_c(2), shape_c(1), one, &
+                      nin_local_xy, nout_local_xy, &
+                      plane_xy(:,:,i), plane_yx(:,:,i), comm_xy, FFTW_ESTIMATE)
+    call fftw_mpi_execute_r2r(transpose_plan, plane_xy(:,:,i), plane_yx(:,:,i))
+  enddo
+  deallocate(plane_xy)
+
+  do idx = 0, nout_local_xy - 1
+    do idy = 0, shape_x(2) - 1
+      do idz = 0, nin_local_yz - 1
+        ieg = 1 + idx + idx_out_local_xy + shape_x(1)*idy &
+                + shape_x(1) * shape_x(2) * (idz + idx_in_local_yz)
+        err = abs(plane_yx(idy,idx,idz) - ieg)
+        if (err > 0.001) then
+          write(*,'(A,6(I6))') "WARNING: confused about k after xy", nid, idx, idy, idz, ieg, int(plane_yx(idy,idx,idz))
+          return
+        endif
+      enddo
+    enddo
+  enddo
+  if (nid == 0) write(*,*) "Passed xy transpose"
+
+  allocate(plane_zy(0:shape_c(3)-1, 0:nout_local_xy-1, 0:nout_local_yz-1) )
+  do i = 0, nout_local_xy-1
+    transpose_plan = fftw_mpi_plan_many_transpose( &
+                      shape_c(3), shape_c(2), one, &
+                      FFTW_MPI_DEFAULT_BLOCK, FFTW_MPI_DEFAULT_BLOCK, &
+                      plane_yx(:,i,:), plane_zy(:,i,:), comm_yz, FFTW_ESTIMATE)
+    call fftw_mpi_execute_r2r(transpose_plan, plane_yx(:,i,:), plane_zy(:,i,:))
+  enddo
+  deallocate(plane_yx)
+
+  do idx = 0, nout_local_xy - 1
+    do idy = 0, nout_local_yz - 1
+      do idz = 0, shape_x(3) - 1
+        ieg = 1 + idx + idx_out_local_xy + shape_x(1)*(idy + idx_out_local_yz) &
+                + shape_x(1) * shape_x(2) * idz
+        err = abs(plane_zy(idz,idx,idy) - ieg)
+        if (err > 0.001) then
+          write(*,'(A,6(I6))') "WARNING: confused about k after yz", nid, idx, idy, idz, ieg, int(plane_zy(idz,idy,idx))
+          return
+        endif
+      enddo
+    enddo
+  enddo
+  if (nid == 0) write(*,*) "Passed yz transpose"
+ 
+end subroutine transpose_test
 
 end module poisson

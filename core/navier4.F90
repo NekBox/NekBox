70c2c26e09976d1041a152f8db46e809baa6d914
Max Hutchinson <maxhutch@gmail.com>
2015-01-13 10:46:08 -0600

    Cleaning, documentation.  Nothing substantial.

--- core/navier4.F90
+++ core/navier4.F90
@@ -1,28 +1,45 @@
 !-----------------------------------------------------------------------
-!> \brief THE ROUTINES BELOW ARE THE NEW Helmholtz projectors
+!> \file navier4.F90
+!! \brief Module for projecting out solutions before Helmholtz solves 
+!! \date January 2015
+!! \author Max Hutchinson
+!!
+!! This module provides hsolve, a Helmholtz (or Poisson) solver that
+!! additionally takes an approximation space and projects out the 
+!! portion of the residual that resides in that space.  The projectors
+!! are generally taken to be past solutions to the same Helmholtz
+!! problem, and the oldest solution is replaced with the current solution
+!! at the end of hsolve.
 !-----------------------------------------------------------------------
-
 module helmholtz
   use kinds, only : DP
   implicit none
 
-  private
 
   public :: hsolve, approx_space
+  private :: projh, gensh, hconj, updrhsh, hmhzpf
 
-
+  !> Type to hold the approximation space.
+  !! Should not be modified outside this module, so more of a handle
   type approx_space
-    real(DP), allocatable :: projectors(:,:)
-    integer :: n_max
-    integer :: n_sav
-    integer :: next
-    real(DP), allocatable :: A_red(:,:) 
+    real(DP), allocatable :: projectors(:,:) !>!< past solutions that span approx. space
+    integer :: n_max !>!< Maximum number of projectors
+    integer :: n_sav !>!< Actual number of projectors
+    integer :: next !>!< Next projector slot to fill in  
+
+    !> Reduced rep of the matrix operator in the approximation space
+    real(DP), allocatable :: H_red(:,:) 
   end type approx_space
 
 contains
 
-!> \brief Orthogonalize the rhs wrt previous rhs's for which we already
-!! know the soln.
+!> \brief Project out the part of the residual in the approx space.
+!!
+!! Starts by finding the EVD H_red to get orthogonal projectors.
+!! Then, computes the overlap of the residual with each projector
+!! and mixes them based on the EVD to get an orthogonal projection.
+!! Next, multiplies by 1/\lambda to take the inverse and expands
+!! back into the full space to populate the approximate solution: projectors(:,0) 
 subroutine projh(r,h1,h2,bi,vml,vmk, apx, wl,ws,name4)
   use kinds, only : DP
   use size_m, only : nx1, ny1, nz1, nelv, nid
@@ -31,21 +48,20 @@ subroutine projh(r,h1,h2,bi,vml,vmk, apx, wl,ws,name4)
   use parallel, only : nid
   implicit none
 
-  real(DP), intent(inout) :: r(*) !>!< residual
-  real(DP), intent(in)    :: h1(*) !>!< coefficient of A (stiffness)
-  real(DP), intent(in)    :: h2(*) !>!< coefficient of M (mass)
+  real(DP), intent(inout) :: r(*)   !>!< residual
+  real(DP), intent(in)    :: h1(*)  !>!< coefficient of A (stiffness)
+  real(DP), intent(in)    :: h2(*)  !>!< coefficient of M (mass)
   real(DP), intent(in)    :: vml(*) !>!< multiplicity array
   real(DP), intent(in)    :: vmk(*) !>!< mask array
-  real(DP), intent(in)    :: bi(*) !>!< inverse mass matrix
-  real(DP), intent(out)   :: wl(*) !>!< large work array (size lx1*ly1*lz1*nelv)
-  real(DP), intent(out)   :: ws(*) !>!< small work array (size 2*max vecs)
-  type(approx_space), intent(inout) :: apx
-  character(4) :: name4
+  real(DP), intent(in)    :: bi(*)  !>!< inverse mass matrix
+  real(DP), intent(out)   :: wl(*)  !>!< large work array (size lx1*ly1*lz1*nelv)
+  real(DP), intent(out)   :: ws(*)  !>!< small work array (size 2*max vecs)
+  type(approx_space), intent(inout) :: apx !>!< Current approx space
+  character(4), intent(in) :: name4 !>!< Name of field for debug printing
 
-  integer :: nel, ntot, i, j, n10
+  integer :: nel, ntot, i, n10
   real(DP) :: vol, alpha1, alpha2, ratio
-  real(DP), external :: glsc23, vlsc3
-  real(DP), external :: glsc2, glsc3
+  real(DP), external :: glsc23
   real(DP), allocatable :: evecs(:,:), work(:), ev(:)
   integer :: lwork, ierr
   real(DP), parameter :: one = 1._dp, zero = 0._dp
@@ -61,27 +77,27 @@ subroutine projh(r,h1,h2,bi,vml,vmk, apx, wl,ws,name4)
   vol = voltm1
   if (nel == nelv) vol = volvm1
 
-!   Diag to see how much reduction in the residual is attained.
-
+  ! Diag to see how much reduction in the residual is attained.
   alpha1 = glsc23(r,bi,vml,ntot)
   if (alpha1 > 0) alpha1 = sqrt(alpha1/vol)
 
-!   Update approximation space if dt has changed
-!  call updrhsh(apx,h1,h2,vml,vmk,ws,name4)
+  ! Update approximation space if dt has changed
+  call updrhsh(apx,h1,h2,vml,vmk,ws)
 
+  !> \note This dsyev call and the following dgemv are task-parallel!
 
-  allocate(evecs(apx%n_sav, apx%n_sav))
-  evecs = apx%A_red(1:apx%n_sav,1:apx%n_sav)
-
-  lwork = 10 * apx%n_sav + 100
-  allocate(work(lwork), ev(apx%n_sav))
+  ! Orthogonalize the approximation space
+  if (10 * apx%n_sav + 100 > ntot) write(*,*) "wl isn't big enough to be dsyev's work"
+  allocate(evecs(apx%n_sav, apx%n_sav), ev(apx%n_sav))
+  evecs = apx%H_red(1:apx%n_sav,1:apx%n_sav)
   call dsyev('V', 'U', apx%n_sav, &
              evecs, apx%n_sav, &
              ev, &
-             work, lwork, ierr) 
+             wl, ntot, ierr) 
   if (nid == 0 .and. ierr /= 0) write(*,*) "DSYEV failed", ierr
-  wl(1:ntot) = r(1:ntot) * vml(1:ntot)
 
+  ! Compute overlap of residual and (non-orthogonal) projectors
+  wl(1:ntot) = r(1:ntot) * vml(1:ntot)
 #if 0
   do i = 1, apx%n_sav
     !ws(i) = glsc3(wl, approx(:,i,1), vml, ntot)
@@ -94,27 +110,33 @@ subroutine projh(r,h1,h2,bi,vml,vmk, apx, wl,ws,name4)
              zero, ws, 1)
   call gop(ws, ws(1+apx%n_sav), '+  ', apx%n_sav)
 #endif
- 
+
+  ! Mix the overlaps to get the orthogonal projection
+  ! and take the inverse by dividing by \lambda 
   do i = 1, apx%n_sav
     ev(i) = sum(evecs(:,i) * ws(1:apx%n_sav)) / ev(i)
   enddo
 
+  ! Compute the weights for the approximate solution
   do i = 1, apx%n_sav
     ws(i) = sum(evecs(i,:) * ev(:))
   enddo
 
+  ! Expand the approximate solution wrt (non-orth.) projectors
   call dgemv('N', ntot, apx%n_sav, &
              one,  apx%projectors(:,1:apx%n_sav), ntot, &
                    ws, 1, &
              zero, apx%projectors(:,0), 1)
 
+  ! Compute the new residual explicitly
+  ! This fixes any numerical precision issues in the previous sections
   call axhelm  (wl,apx%projectors(:,0),h1,h2,1,1)
   wl(1:ntot) = wl(1:ntot) * vmk(1:ntot)
   call dssum   (wl)
   r(1:ntot) = r(1:ntot) - wl(1:ntot)
 
-!...............................................................
-! Diag.
+  !...............................................................
+  ! Recompute the norm of the residual to show how much its shrunk
   alpha2 = glsc23(r,bi,vml,ntot)
   if (alpha2 > 0) alpha2 = sqrt(alpha2/vol)
   ratio  = alpha1/alpha2
@@ -131,158 +153,120 @@ end subroutine projh
 
 !-----------------------------------------------------------------------
 !> \brief Reconstruct the solution to the original problem by adding back
-!!     the previous solutions
-subroutine gensh(v1,h1,h2,vml,vmk,apx,ws,name4)
+!!     the approximate solution, add solution to approximation space.
+subroutine gensh(v1,h1,h2,vml,vmk,apx,ws)
   use kinds, only : DP
-  use size_m, only : nx1, ny1, nz1
-  use size_m, only : lx1, ly1, lz1
-  use parallel, only : nid
   use mesh, only : niterhm
-  use tstep, only : nelfld, ifield
   implicit none
 
-  REAL(DP), intent(inout) :: V1   (LX1,LY1,LZ1,*)
-  REAL(DP), intent(in)    :: H1   (LX1,LY1,LZ1,*)
-  REAL(DP), intent(in)    :: H2   (LX1,LY1,LZ1,*)
-  REAL(DP), intent(in)    :: vmk  (*)
-  REAL(DP), intent(in)    :: vml  (*)
-  type(approx_space), intent(inout) :: apx
-  real(DP), intent(out)   :: ws(:) !>!< workspace?
+  REAL(DP), intent(inout) :: V1 (*) !>!< Full solution
+  REAL(DP), intent(in)    :: H1 (*) !>!< coefficient of A (stiffness)
+  REAL(DP), intent(in)    :: H2 (*) !>!< coefficient of M (mass)
+  REAL(DP), intent(in)    :: vmk(*) !>!< multiplicity array
+  REAL(DP), intent(in)    :: vml(*) !>!< mask array
+  real(DP), intent(out)   :: ws(:)  !>!< small workspace to pass-through
+  type(approx_space), intent(inout) :: apx !>!< current approximation space
 
-  character(4), intent(in) :: name4
+  integer :: ntot
+  ntot = size(apx%projectors,1)
 
-  real(DP) :: alpha, eps
-  real(DP), external :: glsc2, glsc3
-  integer :: ntot, ierr, i
+  ! Reconstruct solution 
+  v1(1:ntot) = v1(1:ntot) + apx%projectors(:,0)
 
-  ntot=nx1*ny1*nz1*nelfld(ifield)
+  ! If the new vector is in the space already, don't re-add it.
+  if (niterhm < 1) return      
 
-!   Reconstruct solution and save current du
-
-  if (apx%n_sav < apx%n_max) then
-  
-      if (niterhm > 0) then      ! new vector not in space
-          apx%n_sav = apx%n_sav+1
-          v1(:,:,:,1:nelfld(ifield)) = v1(:,:,:,1:nelfld(ifield))  &
-                                     + reshape(apx%projectors(:,0), (/lx1,ly1,lz1,nelfld(ifield)/))
-          call copy(apx%projectors(:,apx%n_sav),v1,ntot)
-          call hconj(apx,apx%n_sav,h1,h2,vml,vmk,ws,name4,ierr)
-          apx%next = mod(apx%n_sav, apx%n_max) + 1
-      else
-        if (nid == 0) write(*,*) "Freak out!" 
-      endif
-  else
-      v1(:,:,:,1:nelfld(ifield)) = v1(:,:,:,1:nelfld(ifield))  &
-                                 + reshape(apx%projectors(:,0), (/lx1,ly1,lz1,nelfld(ifield)/))
-      call copy(apx%projectors(:,apx%next),v1,ntot)
-      call hconj(apx,apx%next,h1,h2,vml,vmk,ws,name4,ierr)
-      apx%next = mod(apx%next, apx%n_max) + 1
-  endif
+  ! Add the solution to the approximation space
+  apx%n_sav = min(apx%n_sav + 1, apx%n_max)
+  apx%next  = mod(apx%next, apx%n_max) + 1
+  call copy(apx%projectors(:,apx%next),v1,ntot)
+  call hconj(apx,apx%next,h1,h2,vml,vmk,ws)
 
   return
 end subroutine gensh
 
 !-----------------------------------------------------------------------
-!> \brief Orthonormalize the kth vector against vector set
-subroutine hconj(apx,k,h1,h2,vml,vmk,ws,name4,ierr)
-  use kinds, only : DP
-  use size_m, only : nx1, ny1, nz1, nid
-  use parallel, only : wdsize
-  use tstep, only : istep, ifield, nelfld
+!> \brief Update the k-th row/column of H_red 
+subroutine hconj(apx,k,h1,h2,vml,vmk,ws)
+  use kinds,  only : DP
   implicit none
 
-  integer, intent(in) :: k
-  type(approx_space), intent(inout) :: apx
-  real(DP), intent(in) :: h1(*),h2(*),vml(*),vmk(*)
-  real(DP), intent(out) :: ws(*)
-  character(4) :: name4
-  integer, intent(out) :: ierr
+  type(approx_space), intent(inout) :: apx !>!< Current approximation space
+  integer,  intent(in) :: k      !>!< Index of new projector
+  real(DP), intent(in) :: h1(*)  !>!< coefficient of A (stiffness)
+  real(DP), intent(in) :: h2(*)  !>!< coefficient of M (mass)
+  real(DP), intent(in) :: vml(*) !>!< multiplicity array
+  real(DP), intent(in) :: vmk(*) !>!< mask array
+  real(DP), intent(out) :: ws(*) !>!< small workspace
 
-  integer :: i, ntot, km1 , nel
-  real(DP) :: alpha, ratio, eps, alpham, alph1
-  real(DP), external :: glsc2, ddot
+  integer :: i, ntot
   real(DP), parameter :: one = 1._dp, zero = 0._dp
 
-  ierr=0
-  nel = nelfld(ifield)
-  ntot=nx1*ny1*nz1*nel
+  ntot= size(apx%projectors, 1)
 
+  ! Compute H| projectors(:,k) >
   call axhelm  (apx%projectors(:,0),apx%projectors(:,k),h1,h2,1,1)
   apx%projectors(:,0) = apx%projectors(:,0) * vmk(1:ntot)
   call dssum   (apx%projectors(:,0))
   apx%projectors(:,0) = apx%projectors(:,0) * vml(1:ntot)
 
+  ! Compute < projectors(:,i) | H | projectors(:,k) > for i \in [1,n_sav]
   call dgemv('T', ntot, apx%n_sav, &
              one,  apx%projectors(:,1:apx%n_sav), ntot, &
                    apx%projectors(:,0), 1, &
-             zero, apx%A_red(:,k), 1)
-  call gop(apx%A_red(:,k), ws, '+  ', apx%n_sav)
+             zero, apx%H_red(:,k), 1)
+  call gop(apx%H_red(:,k), ws, '+  ', apx%n_sav)
 
+  ! Re-symmetrize
   do i = 1, apx%n_sav
-    apx%A_red(k,i) = apx%A_red(i,k)
+    apx%H_red(k,i) = apx%H_red(i,k)
   enddo
 
   return
 end subroutine hconj
 
 !-----------------------------------------------------------------------
-!> \brief Reorthogonalize approx if dt has changed
-subroutine updrhsh(apx,h1,h2,vml,vmk,ws,name4)
+!> \brief Recompute H_red if dt has changed
+subroutine updrhsh(apx,h1,h2,vml,vmk,ws)
   use kinds, only : DP
   use size_m, only : lx1, ly1, lz1, lelt, nx1, ny1, nz1
   use input, only : ifvarp, iflomach
   use tstep, only : dt, ifield, nelfld
   implicit none
 
-  integer, parameter :: lt=lx1*ly1*lz1*lelt
-  type(approx_space), intent(inout) :: apx
-  real(DP), intent(in)    :: h1(1),h2(1),vml(1),vmk(1)
-  real(DP), intent(out) :: ws(1)
-  character(4) :: name4
+  type(approx_space), intent(inout) :: apx !>!< current approximation space
+  real(DP), intent(in) :: h1(*)  !>!< coefficient of A (stiffness)
+  real(DP), intent(in) :: h2(*)  !>!< coefficient of M (mass)
+  real(DP), intent(in) :: vml(*) !>!< multiplicity array
+  real(DP), intent(in) :: vmk(*) !>!< mask array
+  real(DP), intent(out) :: ws(*) !>!< small workspace
 
   logical :: ifupdate
   logical, save :: ifnewdt = .false.
-  integer :: n_sav, l, k, ntot, ierr
-
-  character(4), save :: name_old = 'DMIR'
+  integer :: n_sav, k
 
   real(DP), save :: dtold = 0.0
 
-!   First, we have to decide if the dt has changed.
-
+  ! First, we have to decide if the dt has changed.
   ifupdate = .FALSE. 
   if (dt /= dtold) then
       dtold    = dt
-      name_old = name4
       ifnewdt  = .TRUE. 
       ifupdate = .TRUE. 
   elseif (ifnewdt) then
-      if (name4 == name_old) then
-          ifnewdt = .FALSE. 
-      else
-          ifupdate = .TRUE. 
-      endif
+      ifnewdt = .FALSE. 
   endif
   if (ifvarp(ifield)) ifupdate = .TRUE. 
   if (iflomach)       ifupdate = .TRUE. 
 
-  if (ifupdate) then    ! reorthogonalize
-      n_sav = apx%n_sav 
-      l     = 1
-      do k=1,n_sav
-#if 0
-      !           Orthogonalize kth vector against {v_1,...,v_k-1}
-          if (k /= l) then
-              ntot = nx1*ny1*nz1*nelfld(ifield)
-              call copy(approx(1,l),approx(1,k),ntot)
-          endif
-          call hconj(approx,l,h1,h2,vml,vmk,ws,name4,ierr)
-          if (ierr == 0) l=l+1
-#else
+  ! If it has, recompute apx%H_red column by column
+  if (ifupdate) then  
+    n_sav = apx%n_sav 
+    ! Loops over columns to update
+    do k=1,n_sav
       apx%n_sav = k 
-      call hconj(apx,apx%n_sav,h1,h2,vml,vmk,ws,name4,ierr)
-#endif 
-      enddo
+      call hconj(apx, apx%n_sav, h1,h2,vml,vmk,ws)
+    enddo
   endif
 
   return
@@ -344,7 +328,7 @@ end subroutine hmhzpf
 subroutine hsolve(name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd &
     ,apx,bi)
   use kinds, only : DP
-  use size_m, only : lx1, ly1, lz1, lelt, nx1, ny1, nz1, lelv
+  use size_m, only : lx1, ly1, lz1, lelt, lelv
   use parallel, only : nid
   use input, only : ifflow, param
   use string, only : capit
@@ -364,7 +348,7 @@ subroutine hsolve(name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd &
   real(DP), intent(in)    :: tol                  !>!< residual tolerance
   integer,  intent(in)    :: maxit                !>!< maximum number of iterations
   integer,  intent(in)    :: isd                  !>!< something to do with axi-symmetric
-  type(approx_space), intent(inout) :: apx
+  type(approx_space), intent(inout) :: apx !>!< current approximation space
   REAL(DP), intent(in)    :: bi   (LX1,LY1,LZ1,*) !>!< inverse of mass matrix
 
   real(DP), allocatable :: w1(:)
@@ -380,10 +364,12 @@ subroutine hsolve(name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd &
 
   ! figure out if we're projecting or not
   ifstdh = .TRUE. 
+  ! Is this a pressure solve?
   if (cname == 'PRES') then
     if (param(95) /= 0 .AND. istep > param(95) .and. param(93) > 0) then
       ifstdh = .FALSE.
     endif
+  ! Is this a velocity solve?
   elseif (cname == 'VELX' .or. cname == 'VELY' .or. cname == 'VELZ') then
     if (param(94) /= 0 .AND. istep > param(94) .and. param(92) > 0) then
       ifstdh = .FALSE. 
@@ -398,18 +384,17 @@ subroutine hsolve(name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd &
   else
 
       nel = nelfld(ifield)
-      n = nx1*ny1*nz1*nel
 
       call dssum  (r)
       r(:,:,:,1:nel) = r(:,:,:,1:nel) * vmk(:,:,:,1:nel)
 
       allocate(w2(2+2*apx%n_max))
-      allocate(w1(lx1*ly1*lz1*lelt))
+      allocate(w1(lx1*ly1*lz1*lelv))
       call projh  (r,h1,h2,bi,vml,vmk,apx,w1,w2,name)
       deallocate(w1)
 
       call hmhzpf (name,u,r,h1,h2,vmk,vml,imsh,tol,maxit,isd,bi)
-      call gensh  (u,h1,h2,vml,vmk,apx,w2,name)
+      call gensh  (u,h1,h2,vml,vmk,apx,w2)
 
   endif
 

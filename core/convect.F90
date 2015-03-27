fbacb8819356895f2dc8cd3d98e79d783684e154
Max Hutchinson <maxhutch@gmail.com>
2015-03-27 14:26:04 +0000

    Need to use jgl, jgt, dgl, dgt in a few places, so moved to module.

--- core/convect.F90
+++ core/convect.F90
@@ -7,6 +7,7 @@
 !                     <  1.20 for N=16
 !                     ~  1.16 for N=256
 !-----------------------------------------------------------------------
+#define INLINE_INTP
 subroutine setup_convect(igeom)
   use dealias, only : vxd, vyd, vzd
   use input, only : param, ifchar, ifcons, ifpert
@@ -45,255 +46,7 @@ subroutine setup_convect(igeom)
   return
 end subroutine setup_convect
 !-----------------------------------------------------------------------
-!> \brief GLL interpolation from mx to md.
-!! If idir ^= 0, then apply transpose operator  (md to mx)
-subroutine intp_rstd(ju,u,mx,md,if3d,idir) ! GLL->GL interpolation
-  use kinds, only : DP
-  use size_m, only : lxd, ldim
-  use ctimer, only : nintp, tintp, intp_flop, intp_mop, dnekclock
-  implicit none
-
-  real(DP), intent(out) :: ju(*)
-  real(DP), intent(in)  :: u(*)
-  integer,  intent(in)  :: mx, md, idir
-  logical,  intent(in)  :: if3d
-
-  integer, parameter :: ldg=lxd**3
-  real(DP), save :: jgl(ldg), jgt(ldg)
-
-  integer, parameter :: ld=2*lxd
-  real(DP) :: w(ld**ldim,2), etime
-  integer :: ldw, i
-
-!  nintp = nintp + 1
-!  intp_flop = intp_flop + 2*(mx*mx*mx*md + mx*mx*md*md + mx*md*md*md)
-!  intp_mop = intp_mop + mx*mx*mx + md*md*md
-!  etime = dnekclock() 
-
-  call lim_chk(md,ld,'md   ','ld   ','grad_rstd ')
-  call lim_chk(mx,ld,'mx   ','ld   ','grad_rstd ')
-
-  ldw = 2*(ld**ldim)
-
-  call get_int_ptr (i, jgl, jgt, mx,md)
-
-  if (idir == 0) then
-      call specmpn(ju,md,u,mx,jgl(i),jgt(i),if3d,w,ldw)
-  else
-      call specmpn(ju,mx,u,md,jgt(i),jgl(i),if3d,w,ldw)
-  endif
-!  tintp = tintp + (dnekclock() - etime)
-
-  return
-end subroutine intp_rstd
-!-----------------------------------------------------------------------
-!> \brief Generate interpolation from np GLL points to mp GL points
-!!   jgl  = interpolation matrix, mapping from velocity nodes to pressure
-!!   jgt  = transpose of interpolation matrix
-!!   w    = work array of size (np+mp)
-!!   np   = number of points on GLL grid
-!!   mp   = number of points on GL  grid
-subroutine gen_int(jgl,jgt,mp,np,w)
-  use kinds, only : DP
-  use speclib, only : zwgl, zwgll
-  implicit none
-
-  integer, intent(in) :: mp, np
-  real(DP) :: jgl(mp,np),jgt(np*mp),w(*)
-
-  integer :: iz, id, n, i, j
-
-  iz = 1
-  id = iz + np
-
-  call zwgll (w(iz),jgt,np)
-  call zwgl  (w(id),jgt,mp)
-
-  n  = np-1
-  do i=1,mp
-      call fd_weights_full(w(id+i-1),w(iz),n,0,jgt)
-      do j=1,np
-          jgl(i,j) = jgt(j)                  !  Interpolation matrix
-      enddo
-  enddo
-
-  call transpose(jgt,np,jgl,mp)
-
-  return
-end subroutine gen_int
-!-----------------------------------------------------------------------
-!> \brief Generate derivative from np GL points onto mp GL points
-!!  dgl  = interpolation matrix, mapping from velocity nodes to pressure
-!!  dgt  = transpose of interpolation matrix
-!!  w    = work array of size (3*np+mp)
-!!  np   = number of points on GLL grid
-!!  mp   = number of points on GL  grid
-subroutine gen_dgl(dgl,dgt,mp,np,w)
-  use kinds, only : DP
-  use speclib, only : zwgl
-  implicit none
-
-  integer, intent(in) :: mp, np
-  real(DP) :: dgl(mp,np),dgt(np*mp),w(*)
-
-  integer :: iz, id, ndgt, ldgt, n, i, j
-
-  iz = 1
-  id = iz + np
-
-  call zwgl  (w(iz),dgt,np)  ! GL points
-  call zwgl  (w(id),dgt,mp)  ! GL points
-
-  ndgt = 2*np
-  ldgt = mp*np
-  call lim_chk(ndgt,ldgt,'ldgt ','dgt  ','gen_dgl   ')
-
-  n  = np-1
-  do i=1,mp
-      call fd_weights_full(w(id+i-1),w(iz),n,1,dgt) ! 1=1st deriv.
-      do j=1,np
-          dgl(i,j) = dgt(np+j)                       ! Derivative matrix
-      enddo
-  enddo
-
-  call transpose(dgt,np,dgl,mp)
-
-  return
-end subroutine gen_dgl
-!-----------------------------------------------------------------------
-!> Check array limits
-subroutine lim_chk(n,m,avar5,lvar5,sub_name10)
-  use size_m, only : nid            ! need nid
-  implicit none
-  character(5), intent(in) ::  avar5,lvar5
-  character(10), intent(in) :: sub_name10
-  integer, intent(in) :: n, m
-
-  if (n > m) then
-      write(6,1) nid,n,m,avar5,lvar5,sub_name10
-      1 format(i8,' ERROR: :',2i12,2(1x,a5),1x,a10)
-      call exitti('lim_chk problem. $',n)
-  endif
-
-  return
-end subroutine lim_chk
-!-----------------------------------------------------------------------
-!> \brief Get pointer to jgl() for interpolation pair (mx,md)
-!!
-!! The interpolation matrices jgl, jgt are being memoized.
-!!  pjgl is a map from (mx,md) pair to (ip) index of jgl, jgt
-subroutine get_int_ptr (ip, jgl, jgt, mx,md) ! GLL-->GL pointer
-  use kinds, only : DP
-  use size_m
-  implicit none
-
-  integer, parameter :: ldg=lxd**3, lwkd=4*lxd*lxd
-
-  integer, intent(out) :: ip
-  real(DP), intent(inout) :: jgl(ldg), jgt(ldg)
-  integer, intent(in) :: mx, md
-
-  real(DP) :: wkd(lwkd)
-
-  integer, parameter :: ld=2*lxd
-  integer, save :: pjgl(0:ld*ld) = 0
-
-  integer :: ij, nstore, nwrkd
-
-  ij = md + ld*(mx-1)
-  ip = pjgl(ij)
-
-  if (ip == 0) then
-
-      nstore   = pjgl(0)
-      pjgl(ij) = nstore+1
-      nstore   = nstore + md*mx
-      pjgl(0)  = nstore
-      ip       = pjgl(ij)
-  
-      nwrkd = mx + md
-      call lim_chk(nstore,ldg ,'jgl  ','ldg  ','get_int_pt')
-      call lim_chk(nwrkd ,lwkd,'wkd  ','lwkd ','get_int_pt')
-  
-      call gen_int(jgl(ip),jgt(ip),md,mx,wkd)
-  endif
-
-  return
-end subroutine get_int_ptr
-!-----------------------------------------------------------------------
-!> \brief Get pointer to GL-GL interpolation dgl() for pair (mx,md)
-subroutine get_dgl_ptr (ip, dg, dgt, wkd, mx,md)
-  use kinds, only : DP
-  use size_m, only : lxd 
-  implicit none
-
-  integer, parameter :: ld=2*lxd
-  integer, parameter :: ldg=lxd**3, lwkd=4*lxd*lxd
-
-  integer, intent(out) :: ip
-  real(DP), intent(out) :: dg(ldg), dgt(ldg), wkd(lwkd)
-  integer, intent(in) :: mx, md
-
-  integer, save ::  pdg   (0:ld*ld)
-
-  integer :: ij, nstore, nwrkd
-
-  ij = md + ld*(mx-1)
-  ip = pdg (ij)
-
-  if (ip == 0) then
-
-      nstore   = pdg (0)
-      pdg (ij) = nstore+1
-      nstore   = nstore + md*mx
-      pdg (0)  = nstore
-      ip       = pdg (ij)
-  
-      nwrkd = mx + md
-      call lim_chk(nstore,ldg ,'dg   ','ldg  ','get_dgl_pt')
-      call lim_chk(nwrkd ,lwkd,'wkd  ','lwkd ','get_dgl_pt')
-  
-      call gen_dgl(dg (ip),dgt(ip),md,mx,wkd)
-  endif
-
-  return
-end subroutine get_dgl_ptr
-!-----------------------------------------------------------------------
-subroutine grad_rst(ur,us,ut,u,md,if3d) ! Gauss-->Gauss grad
-  use kinds, only : DP
-  use ctimer, only : ngrst, tgrst, grst_flop, grst_mop, dnekclock
-  use size_m
-  implicit none
-
-  real(DP) ::    ur(1),us(1),ut(1),u(1)
-  integer, intent(in) :: md
-  logical :: if3d
-
-  integer, parameter :: ldg=lxd**3, lwkd=4*lxd*lxd
-  real(DP), save :: dg(ldg), dgt(ldg)
-  real(DP) ::  wkd(lwkd)
-  integer :: m0, ip
-  real(DP) :: etime
-
-!  ngrst = ngrst + 1
-!  grst_flop = grst_flop + 3*((2*md-1)*md**3)
-!  grst_mop  = grst_mop  + 4*md**3
-
-!  etime = dnekclock()
-
-  m0 = md-1
-  call get_dgl_ptr (ip, dg, dgt, wkd, md,md)
-  if (if3d) then
-      call local_grad3(ur,us,ut,u,m0,1,dg(ip),dgt(ip))
-  else
-!max        call local_grad2(ur,us   ,u,m0,1,dg(ip),dgt(ip))
-  endif
-!  tgrst = tgrst + (dnekclock() - etime)
-  return
-end subroutine grad_rst
-!-----------------------------------------------------------------------
 !> \brief Compute dealiased form:  J^T Bf *JC .grad Ju w/ correct Jacobians
-#define INLINE_INTP
 subroutine convect_new(bdu,u,ifuf,cx,cy,cz,ifcf)
   use kinds, only : DP
   use size_m, only : nelv
@@ -301,6 +54,8 @@ subroutine convect_new(bdu,u,ifuf,cx,cy,cz,ifcf)
   use size_m, only : nx1, ny1, nz1, nxd, nyd, nzd
   use input, only : if3d
   use ctimer, only : tscn, dnekclock
+  use interp, only : jgl, jgt, dgl, dgt
+  use interp, only : get_int_ptr, get_dgl_ptr
   implicit none
 
   real(DP) :: bdu(*),u(*),cx(*),cy(*),cz(*)
@@ -312,12 +67,8 @@ subroutine convect_new(bdu,u,ifuf,cx,cy,cz,ifcf)
   integer :: e, iu, ic, ib, i, iptr, iptr2
   integer :: nxyz1, nxyzd, nxyzu, nxyzc
   real(DP) :: etime
-
-  integer, parameter :: ldg = lxd**3
-  real(DP), save :: jgl(ldg), jgt(ldg)
-  real(DP), save :: dgl(ldg), dgt(ldg)
-  real(DP) :: w((2*lxd)**ldim,2), wkd(4*lxd*lxd)
-  integer, parameter :: ldw = 2*(2*lxd)**ldim
+  real(DP) :: w((2*lxd)**ldim,2)
+  integer, parameter :: ldw = 2*(2*lxd)**2
 
   etime = dnekclock()
 !max  call set_dealias_rx()
@@ -336,8 +87,8 @@ subroutine convect_new(bdu,u,ifuf,cx,cy,cz,ifcf)
   ib = 1    ! pointer to scalar field Bdu
 
 #ifdef INLINE_INTP
-  call get_int_ptr (iptr, jgl, jgt, nx1,nxd)
-  call get_dgl_ptr (iptr2, dgl, dgt, wkd, nxd,nxd)
+  call get_int_ptr (iptr,  nx1,nxd)
+  call get_dgl_ptr (iptr2, nxd,nxd)
 #endif
 
   do e=1,nelv
@@ -429,23 +180,23 @@ subroutine set_convect_new(cr,cs,ct,ux,uy,uz)
   use input, only : if3d
   use mesh, only : if_ortho
   use ctimer, only : tscn, nscn, dnekclock
+  use interp, only : jgl, jgt
+  use interp, only : get_int_ptr
+
   implicit none
 
   integer, parameter :: lxy=lx1*ly1*lz1, ltd=lxd*lyd*lzd
-  integer, parameter :: ld=2*lxd
-  integer, parameter :: ldw=2*(ld**ldim)
 
   real(DP) :: cr(ltd,*),cs(ltd,*),ct(ltd,*)
   real(DP) :: ux(lxy,*),uy(lxy,*),uz(lxy,*)
 
   real(DP) :: fx(ltd), fy(ltd), fz(ltd)!, ur, us, ut, tr, uf
-  real(DP) :: w(ld**ldim,2)
+  real(DP) :: w((2*lxd)**ldim,2)
+  integer, parameter :: ldw = 2*(2*lxd)**2
+
   real(DP) :: etime
-  integer, parameter :: ldg=lxd**3
-  !real(DP), save :: jgl(ldg), jgt(ldg)
 
-
-  integer :: e, nxyz1, nxyzd, ic, i, j
+  integer :: e, nxyz1, nxyzd, ic, i, j, iptr
   nscn = nscn + 1
   etime = dnekclock()
   call set_dealias_rx()
@@ -454,7 +205,9 @@ subroutine set_convect_new(cr,cs,ct,ux,uy,uz)
   nxyzd = nxd*nyd*nzd
 
   ic = 1    ! pointer to vector field C
-  !call get_int_ptr (j, jgl, jgt, nx1,nxd) 
+#ifdef INLINE_INTP
+  call get_int_ptr (iptr, nx1, nxd) 
+#endif
 
   do e=1,nelv
 
@@ -464,9 +217,15 @@ subroutine set_convect_new(cr,cs,ct,ux,uy,uz)
       !call specmpn(fy,nxd,uy(1,e),nx1,jgl(i),jgt(i),if3d,w,ldw)
       !call specmpn(fz,nxd,uz(1,e),nx1,jgl(i),jgt(i),if3d,w,ldw)
 !      etime = etime - dnekclock()
+#ifdef INLINE_INTP
+      call specmpn(fx,nxd,ux(1,e),nx1,jgl(iptr),jgt(iptr),if3d,w,ldw)
+      call specmpn(fy,nxd,uy(1,e),nx1,jgl(iptr),jgt(iptr),if3d,w,ldw)
+      if (if3d) call specmpn(fz,nxd,uz(1,e),nx1,jgl(iptr),jgt(iptr),if3d,w,ldw)
+#else
       call intp_rstd(fx,ux(1,e),nx1,nxd,if3d,0) ! 0 --> forward
       call intp_rstd(fy,uy(1,e),nx1,nxd,if3d,0) ! 0 --> forward
       if (if3d) call intp_rstd(fz,uz(1,e),nx1,nxd,if3d,0) ! 0 --> forward
+#endif
 !      etime = etime + dnekclock()
 
   !        Convert convector F to r-s-t coordinates

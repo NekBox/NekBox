99e3f56d8288d17658ee0380c1ef40b7c1682633
Max Hutchinson <maxhutch@gmail.com>
2015-04-23 14:17:33 -0500

    Trying to get travis to work again.

--- fftw3.F90
+++ fftw3.F90
@@ -1,7 +1,7 @@
 module fftw3
   use, intrinsic :: iso_c_binding
   implicit none
-  include 'fftw3-mpi.f03'
+  include 'fftw3.f03'
   integer, parameter :: plan_kind = 8
   !integer, external :: fftw_mpi_local_size_3d_transposed
 end module fftw3

5bb1bc759aabdc65bd866b24fe8417e37070d637
Max Hutchinson <maxhutch@gmail.com>
2015-05-18 16:45:00 -0500

    Uploading new test.  We'll see if travis likes it.

---
+++ tests/ndiff.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from sys import argv, exit
+import numpy as np
+
+files = []
+files.append(open(argv[1], 'r'))
+files.append(open(argv[2], 'r'))
+vals = [[1]]
+failed = False
+while len(vals[0]) > 0:
+    vals = [np.array([float(x) for x in fp.readline().split()[2:]], dtype=float) for fp in files]
+    diffs = np.maximum(*vals) - np.minimum(*vals)
+    if np.any(diffs > 0):
+        failed = True
+
+if failed:
+    exit(6)
+else:
+    exit(0)

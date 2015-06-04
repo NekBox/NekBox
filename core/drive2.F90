0b91f60bc4247ffaa1f2c3c62572a701fa91c48d
Max Hutchinson <maxhutch@gmail.com>
2015-06-03 22:52:27 -0500

    Flops or mops could also be zero.

--- core/drive2.F90
+++ core/drive2.F90
@@ -1482,9 +1482,9 @@ subroutine print_flops(label, flops, mops, time)
   !real(DP), parameter :: compute = 3400*8
   real(DP) :: peak
 
-  peak = min(compute, flops * bandwidth / (mops*8))
 
-  if (time*peak > 1.e-6) then
+  if (time > 1.e-6 .and. flops > 1 .and. mops > 1) then
+  peak = min(compute, flops * bandwidth / (mops*8))
   write(6,'(A,2F14.1,F8.4)') label, &
                        real(flops, kind=DP) / (1.e6 * time), &
                        peak, real(flops, kind=DP) / (1.e6 * time * peak)

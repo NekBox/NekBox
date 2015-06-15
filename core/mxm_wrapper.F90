d8e48c566021432738054540d1c96b692adb59ab
Max Hutchinson <maxhutch@gmail.com>
2015-06-15 21:24:10 +0000

    Interating more old changes from cetus.

--- core/mxm_wrapper.F90
+++ core/mxm_wrapper.F90
@@ -18,25 +18,25 @@ subroutine mxm(a,n1,b,n2,c,n3)
     integer(i8) :: tt = 32
 
     if (n2 == 8 .and. mod(n1,4) == 0 &
-!        .and. MOD(LOC(a),tt)==0 & 
-!        .and. MOD(LOC(b),tt)==0 & 
-!        .and. MOD(LOC(c),tt)==0 & 
+        .and. MOD(LOC(a),tt)==0 & 
+        .and. MOD(LOC(b),tt)==0 & 
+        .and. MOD(LOC(c),tt)==0 & 
        ) then
      call mxm_bgq_8(a, n1, b, n2, c, n3)  
      return
    endif
    if (n2 == 12 .and. mod(n1,4) == 0 &
-!        .and. MOD(LOC(a),tt)==0 & 
-!        .and. MOD(LOC(b),tt)==0 & 
-!        .and. MOD(LOC(c),tt)==0 & 
+        .and. MOD(LOC(a),tt)==0 & 
+        .and. MOD(LOC(b),tt)==0 & 
+        .and. MOD(LOC(c),tt)==0 & 
        ) then
      call mxm_bgq_12(a, n1, b, n2, c, n3)  
      return
    endif
    if (n2 == 16 .and. mod(n1,4) == 0 &
-!        .and. MOD(LOC(a),tt)==0 & 
-!        .and. MOD(LOC(b),tt)==0 & 
-!        .and. MOD(LOC(c),tt)==0 & 
+        .and. MOD(LOC(a),tt)==0 & 
+        .and. MOD(LOC(b),tt)==0 & 
+        .and. MOD(LOC(c),tt)==0 & 
        ) then
      call mxm_bgq_16(a, n1, b, n2, c, n3)  
      return

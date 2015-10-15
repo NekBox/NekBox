b263757b82026aabf11f2eefe0e0c4f5051754c3
Max Hutchinson <maxhutch@gmail.com>
2015-10-15 13:00:31 -0500

    Switching to legacy FFTW inteface for mkl interop.

--- core/fftw3.F90
+++ core/fftw3.F90
@@ -1,7 +1,6 @@
 module fftw3
   use, intrinsic :: iso_c_binding
   implicit none
-  include 'fftw3.f03'
+  include 'fftw3.f'
   integer, parameter :: plan_kind = 8
-  !integer, external :: fftw_mpi_local_size_3d_transposed
 end module fftw3

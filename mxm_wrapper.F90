258f5318100f8276fc8ae9ae6d2108b5e6e2767f
Max Hutchinson <maxhutch@gmail.com>
2015-11-19 17:21:53 +0100

    Updated to new libxsmm interface; removed direct calls.

--- mxm_wrapper.F90
+++ mxm_wrapper.F90
@@ -6,22 +6,10 @@
 !! A, B, and C must be contiguously packed
 !#define USE_LIBXSMM
 
-
-#ifdef XSMM
-!#define XSMM_DIRECT
-#define XSMM_DISPATCH
-!#include "libxsmm.f"
-#endif
-
 subroutine mxm(a,n1,b,n2,c,n3)
   use kinds, only : DP, i8
 #ifdef XSMM
-#ifdef XSMM_DIRECT
-  use iso_c_binding
-  use libxsmm, only : libxsmm_mm
-#else
   use libxsmm
-#endif
 #endif
   implicit none
     
@@ -31,96 +19,8 @@ subroutine mxm(a,n1,b,n2,c,n3)
   integer :: aligned
   integer :: K10_mxm
 
-#ifdef XSMM_DISPATCH
-  INTEGER, PARAMETER :: T = LIBXSMM_FLD_KIND
-  PROCEDURE(LIBXSMM_DMM_FUNCTION), POINTER :: xmm
-  TYPE(LIBXSMM_DGEMM_XARGS) :: xargs
-  TYPE(C_FUNPTR) :: f
-#endif
-
-#ifdef XSMM_DIRECT
-  interface libxsmm_dmm_8_8_8
-    subroutine libxsmm_dmm_8_8_8(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_64_8_8
-    subroutine libxsmm_dmm_64_8_8(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_8_64_8
-    subroutine libxsmm_dmm_8_64_8(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_4_4_4
-    subroutine libxsmm_dmm_4_4_4(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_4_16_4
-    subroutine libxsmm_dmm_4_16_4(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_16_4_4
-    subroutine libxsmm_dmm_16_4_4(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_10_10_10
-    subroutine libxsmm_dmm_10_10_10(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_10_100_10
-    subroutine libxsmm_dmm_10_100_10(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_100_10_10
-    subroutine libxsmm_dmm_100_10_10(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-
-  interface libxsmm_dmm_144_12_8
-    subroutine libxsmm_dmm_144_12_8(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-
-
-
-  interface libxsmm_dmm_12_12_12
-    subroutine libxsmm_dmm_12_12_12(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_144_12_12
-    subroutine libxsmm_dmm_144_12_12(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
-  interface libxsmm_dmm_12_144_12
-    subroutine libxsmm_dmm_12_144_12(a,b,c) BIND(C)
-      use iso_c_binding, only : c_ptr
-      type(c_ptr), value :: a, b, c
-    end subroutine
-  end interface
+#ifdef XSMM
+  TYPE(LIBXSMM_DMM_FUNCTION) :: xmm
 #endif
 
 #ifdef BGQ
@@ -169,73 +69,11 @@ subroutine mxm(a,n1,b,n2,c,n3)
 #endif
 
 #ifdef XSMM
-
-
-#ifdef XSMM_DISPATCH
-  xargs = LIBXSMM_DGEMM_XARGS_CTOR(1.0_dp, 0.0_dp)
-  f = libxsmm_dispatch(n1, n3, n2, 1.0_dp, 0.0_dp)
-  if (C_ASSOCIATED(f)) then
-      CALL C_F_PROCPOINTER(f, xmm)
-      CALL xmm(a, b, c, xargs)
+  CALL libxsmm_dispatch(xmm, n1, n3, n2, 1.0_dp, 0.0_dp)
+  IF (libxsmm_available(xmm)) then
+      CALL libxsmm_call(xmm, C_LOC(a), C_LOC(b), C_LOC(c))
       return
   endif
-#endif
-
-#ifdef XSMM_DIRECT
-    if (n2 == 8) then
-      if (n1 == 8 .and. n3 == 8) then
-        call libxsmm_dmm_8_8_8(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 64 .and. n3 == 8) then
-        call libxsmm_dmm_64_8_8(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 8 .and. n3 == 64) then
-        call libxsmm_dmm_8_64_8(c_loc(a),c_loc(b),c_loc(c))
-        return
-      endif
-      else if (n1 == 144 .and. n3 == 12) then
-        call libxsmm_dmm_144_12_8(c_loc(a),c_loc(b),c_loc(c))
-        return
-      endif
-    else if (n2 == 12) then
-      if (n1 == 12 .and. n3 == 12) then
-        call libxsmm_dmm_12_12_12(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 144 .and. n3 == 12) then
-        call libxsmm_dmm_144_12_12(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 12 .and. n3 == 144) then
-        call libxsmm_dmm_12_144_12(c_loc(a),c_loc(b),c_loc(c))
-        return
-      endif
-    else if (n2 == 10) then
-      if (n1 == 10 .and. n3 == 10) then
-        call libxsmm_dmm_10_10_10(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 100 .and. n3 == 10) then
-        call libxsmm_dmm_100_10_10(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 10 .and. n3 == 100) then
-        call libxsmm_dmm_10_100_10(c_loc(a),c_loc(b),c_loc(c))
-        return
-      endif
-    else if (n2 == 4) then
-      if (n1 == 4 .and. n3 == 4) then
-        call libxsmm_dmm_4_4_4(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 16 .and. n3 == 4) then
-        call libxsmm_dmm_16_4_4(c_loc(a),c_loc(b),c_loc(c))
-        return
-      else if (n1 == 4 .and. n3 == 16) then
-        call libxsmm_dmm_4_16_4(c_loc(a),c_loc(b),c_loc(c))
-        return
-      endif
-    endif
-#endif
-#ifndef XSMM_DISPATCH
-    call libxsmm_mm(n1, n3, n2, a, b, c)
-    return
-#endif
 #endif
 
 

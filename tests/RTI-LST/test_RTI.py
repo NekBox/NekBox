88859324cf33ead73204082f2f105a2ba9f5a4d0
Max Hutchinson <maxhutch@gmail.com>
2016-04-29 14:12:36 -0500

    Changing test to use io_time; adding failing restart test.

--- tests/RTI-LST/test_RTI.py
+++ tests/RTI-LST/test_RTI.py
@@ -9,6 +9,8 @@ import numpy as np
 with open(join(dirname(__file__), "LST_f90.tusr"), "r") as f:
     tusr = f.read()
 
+base_dir = join(getcwd(), "scratch")
+
 def scalars(lines):
     res = []
     for line in lines:
@@ -27,7 +29,7 @@ def velocities(lines):
 def test_periodic():
     with open(join(dirname(__file__), "LST.json")) as f:
         base = json.load(f)
-    workdir = join(getcwd(), "test_RTI")
+    workdir = join(base_dir, "test_RTI")
     base["prefix"] = "test_RTI"
     config = configure(base, {'name': 'test_RTI'}, workdir)
     res = series(config, tusr)
@@ -49,6 +51,33 @@ def test_periodic():
     refs  = velocities(ref)
     assert tests.shape[0] == refs.shape[0]
     max_velocity_diff = np.max(np.abs(tests-refs))
+    assert max_velocity_diff < 1.e-9
+
+    return
+
+def test_restart():
+    with open(join(dirname(__file__), "LST.json")) as f:
+        base = json.load(f)
+    workdir = join(base_dir, "test_restart")
+    base["prefix"] = "test_restart"
+    config = configure(base, {'name': 'test_restart'}, workdir)
+    res = series(config, tusr, job_time = 0.5)
+    run_all([res,], base)[0]
+
+    with open(join(workdir, "test_restart-1.stdout")) as f:
+        test = f.readlines()    
+
+    with open(join(dirname(__file__), "LST_ref.out")) as f:
+        ref = f.readlines()
+
+    tests = scalars(test)
+    refs  = scalars(ref)
+    max_scalar_diff = np.max(np.abs(tests[-1]-refs[-1]))
+    assert max_scalar_diff < 1.e-9
+
+    tests = velocities(test)
+    refs  = velocities(ref)
+    max_velocity_diff = np.max(np.abs(tests[-1]-refs[-1]))
     assert max_velocity_diff < 1.e-9
 
     return

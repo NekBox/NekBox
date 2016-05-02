b16106b322021a4c929dd993e4d0120e6162f38d
Max Hutchinson <maxhutch@gmail.com>
2016-05-02 14:00:56 -0500

    Adding pytest marker.

--- tests/eddy/test_convergence.py
+++ tests/eddy/test_convergence.py
@@ -6,6 +6,7 @@ from nekpy.dask.utils import work_name, outer_product
 from os.path import join, dirname
 from os import getcwd
 import numpy as np
+import pytest
 
 with open(join(dirname(__file__), "eddy_uv_f90.tusr"), "r") as f:
     tusr = f.read()
@@ -37,6 +38,36 @@ def test_single():
 
     return
 
+@pytest.mark.pr
+def test_bdf2():
+    with open(join(dirname(__file__), "eddy_uv.json")) as f:
+        base = json.load(f)
+    base["prefix"] = "test_bdf2"
+    base["torder"] = 2
+    sweep = {"courant" : [0.5, 0.25, 0.125]}
+    overrides = list(outer_product(sweep))
+    for ov in overrides:
+        ov["name"] = work_name(base["prefix"], ov)
+
+    workdir = join(base_dir, base["prefix"])
+    configs = [configure(base, ov, join(base_dir, ov["name"])) for ov in overrides]
+    res = [series(config, tusr) for config in configs]
+    run_all(res, base)
+
+    errs = {}
+    for config in configs:
+        with open(join(config["workdir"], "{}-0.stdout".format(config["name"]))) as f:
+            test = f.readlines()    
+        errs[config["courant"]] =  np.max(np.abs(get_xerr(test)))
+
+    assert errs[.5] / errs[.25] > 3
+    assert errs[.5] / errs[.25] < 6
+    assert errs[.25] / errs[.125] > 3
+    assert errs[.25] / errs[.125] < 6
+
+    return
+
+@pytest.mark.pr
 def test_bdf3():
     with open(join(dirname(__file__), "eddy_uv.json")) as f:
         base = json.load(f)
@@ -65,17 +96,16 @@ def test_bdf3():
 
     return
 
-def test_bdf2():
+@pytest.mark.pr
+def test_bdf4():
     with open(join(dirname(__file__), "eddy_uv.json")) as f:
         base = json.load(f)
-    base["prefix"] = "test_bdf2"
-    base["torder"] = 2
+    base["prefix"] = "test_bdf4"
+    base["torder"] = 4
     sweep = {"courant" : [0.5, 0.25, 0.125]}
     overrides = list(outer_product(sweep))
     for ov in overrides:
         ov["name"] = work_name(base["prefix"], ov)
-
-    print(overrides)
 
     workdir = join(base_dir, base["prefix"])
     configs = [configure(base, ov, join(base_dir, ov["name"])) for ov in overrides]
@@ -88,10 +118,9 @@ def test_bdf2():
             test = f.readlines()    
         errs[config["courant"]] =  np.max(np.abs(get_xerr(test)))
 
-    assert errs[.5] / errs[.25] > 3
-    assert errs[.5] / errs[.25] < 6
-    assert errs[.25] / errs[.125] > 3
-    assert errs[.25] / errs[.125] < 6
+    assert errs[.5] / errs[.25] > 12
+    assert errs[.5] / errs[.25] < 24
+    assert errs[.25] / errs[.125] > 12
+    assert errs[.25] / errs[.125] < 24
 
     return
-
